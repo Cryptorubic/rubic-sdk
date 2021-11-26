a64f47fca8136f8cc339f7c7f139a27cd60be7b8
ottebrut <ott.andrey.ao@gmail.com>
2021-11-26 17:31:58 +0300

    Rename tokens structs

--- src/core/blockchain/models/blockchain.ts
+++ src/core/blockchain/models/blockchain.ts
@@ -1,8 +1,8 @@
-import { BlockchainToken } from '@core/blockchain/models/blockchain-token';
 import { BLOCKCHAIN_NAME } from '@core/blockchain/models/BLOCKCHAIN_NAME';
+import { Token } from '@core/blockchain/tokens/token';
 
 export interface Blockchain {
     id: number;
     name: BLOCKCHAIN_NAME;
-    nativeCoin: BlockchainToken;
+    nativeCoin: Token;
 }
---
+++ src/core/blockchain/tokens/price-token-amount.ts
@@ -0,0 +1,48 @@
+import { TokenBaseStruct } from '@core/blockchain/models/token-base-struct';
+import { PriceToken } from '@core/blockchain/tokens/price-token';
+import BigNumber from 'bignumber.js';
+
+type PriceTokenAmountStruct = ConstructorParameters<typeof PriceToken>[number] & {
+    weiAmount: BigNumber;
+};
+
+type TokenAmountBaseStruct = TokenBaseStruct & { weiAmount: BigNumber };
+
+export class PriceTokenAmount extends PriceToken {
+    public static async createToken(
+        tokenAmountBaseStruct: TokenAmountBaseStruct
+    ): Promise<PriceToken> {
+        const token = await super.createToken(tokenAmountBaseStruct);
+        return new PriceTokenAmount({
+            ...token.asStruct,
+            weiAmount: tokenAmountBaseStruct.weiAmount
+        });
+    }
+
+    private readonly _weiAmount: BigNumber;
+
+    get weiAmount(): BigNumber {
+        return new BigNumber(this._weiAmount);
+    }
+
+    get stringWeiAmount(): string {
+        return this._weiAmount.toFixed(0);
+    }
+
+    get tokenAmount(): BigNumber {
+        return new BigNumber(this._weiAmount).div(new BigNumber(10).pow(this.decimals));
+    }
+
+    constructor(tokenStruct: PriceTokenAmountStruct) {
+        super(tokenStruct);
+        this._weiAmount = new BigNumber(tokenStruct.weiAmount);
+    }
+
+    public weiAmountMinusSlippage(slippage: number): BigNumber {
+        return new BigNumber(this._weiAmount).multipliedBy(new BigNumber(1).minus(slippage));
+    }
+
+    public weiAmountPlusSlippage(slippage: number): BigNumber {
+        return new BigNumber(this._weiAmount).multipliedBy(new BigNumber(1).plus(slippage));
+    }
+}
