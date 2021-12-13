7b1e0c05b4c2e05af2edb4dbb0b56985074e0a05
siandrev <andreev.sergey.i@yandex.ru>
2021-12-13 10:12:36 +0300

    SwapCalculationOptions interface improved

--- src/features/swap/models/swap-calculation-options.ts
+++ src/features/swap/models/swap-calculation-options.ts
@@ -2,7 +2,5 @@ import { SwapOptions } from '@features/swap/models/swap-options';
 
 export interface SwapCalculationOptions extends SwapOptions {
     readonly gasCalculation: 'disabled' | 'calculate' | 'rubicOptimisation';
-    readonly slippageTolerance: number;
-    readonly deadlineMinutes: number;
     readonly disableMultihops: boolean;
 }
