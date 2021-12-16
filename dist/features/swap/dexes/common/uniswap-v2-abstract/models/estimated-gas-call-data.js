3201bfad8478332aa52ff3d170dcd198b452cca2
siandrev <andreev.sergey.i@yandex.ru>
2021-12-16 23:14:41 +0300

    webpack build added

--- dist/features/swap/dexes/common/uniswap-v2-abstract/models/estimated-gas-call-data.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=estimated-gas-call-data.js.map
--- dist/features/swap/models/swap-manager-calculation-options.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=swap-manager-calculation-options.js.map
