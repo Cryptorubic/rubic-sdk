b58a1cea4dac28aba7937e46c17a3ae3901f1f14
ottebrut <ott.andrey.ao@gmail.com>
2022-01-11 14:26:50 +0300

    Update type guards

--- src/features/swap/type-guards.ts
+++ src/features/swap/type-guards.ts
@@ -1,15 +1,16 @@
 import { UniswapV2AbstractTrade } from '@features/swap/dexes/common/uniswap-v2-abstract/uniswap-v2-abstract-trade';
 import { OneinchTrade } from '@features/swap/dexes/common/oneinch-common/oneinch-trade';
 import { ZrxTrade } from '@features/swap/dexes/common/zrx-common/zrx-trade';
-import { UniSwapV3Trade } from '@features/swap/dexes/ethereum/uni-swap-v3/uni-swap-v3-trade';
 import { InstantTrade } from '@features/swap/instant-trade';
+import { UniswapV3AbstractTrade } from '@features/swap/dexes/common/uniswap-v3-abstract/uniswap-v3-abstract-trade';
+import { AlgebraTrade } from '@features/swap/dexes/polygon/algebra/algebra-trade';
 
 export function isUniswapV2LikeTrade(trade: InstantTrade): trade is UniswapV2AbstractTrade {
     return trade instanceof UniswapV2AbstractTrade;
 }
 
-export function isUniswapV3LikeTrade(trade: InstantTrade): trade is UniSwapV3Trade {
-    return trade instanceof UniSwapV3Trade;
+export function isUniswapV3LikeTrade(trade: InstantTrade): trade is UniswapV3AbstractTrade {
+    return trade instanceof UniswapV3AbstractTrade;
 }
 
 export function isOneInchLikeTrade(trade: InstantTrade): trade is OneinchTrade {
@@ -18,4 +19,8 @@ export function isOneInchLikeTrade(trade: InstantTrade): trade is OneinchTrade {
 
 export function isZrxLikeTradeLikeTrade(trade: InstantTrade): trade is ZrxTrade {
     return trade instanceof ZrxTrade;
+}
+
+export function isAlgebraTrade(trade: InstantTrade): trade is AlgebraTrade {
+    return trade instanceof AlgebraTrade;
 }
