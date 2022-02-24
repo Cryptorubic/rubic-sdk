2de3a36324d8ea9baf1a90348138ec6e91ebfcc8
ottebrut <ott.andrey.ao@gmail.com>
2022-02-24 18:16:32 +0300

    Update errors prototype

--- src/common/errors/cross-chain/cross-chain-max-amount-error.ts
+++ src/common/errors/cross-chain/cross-chain-max-amount-error.ts
@@ -4,5 +4,6 @@ import BigNumber from 'bignumber.js';
 export class CrossChainMaxAmountError extends RubicSdkError {
     constructor(maxAmount: BigNumber, tokenSymbol: string) {
         super(`Max amount is ${maxAmount.toFixed()} ${tokenSymbol}`);
+        Object.setPrototypeOf(this, CrossChainMaxAmountError.prototype);
     }
 }
--- src/common/errors/cross-chain/cross-chain-min-amount-error.ts
+++ src/common/errors/cross-chain/cross-chain-min-amount-error.ts
@@ -4,5 +4,6 @@ import BigNumber from 'bignumber.js';
 export class CrossChainMinAmountError extends RubicSdkError {
     constructor(minAmount: BigNumber, tokenSymbol: string) {
         super(`Min amount is ${minAmount.toFixed()} ${tokenSymbol}`);
+        Object.setPrototypeOf(this, CrossChainMinAmountError.prototype);
     }
 }
