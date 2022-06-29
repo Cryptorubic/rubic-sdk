ae0cab0e4b45280873583dd7e4b2a7fd30e64e77
axtezy <axtezy@gmail.com>
2022-06-29 17:58:22 +0300

    Add wait for complete to symbiosis manager

--- src/features/cross-chain/cross-chain-symbiosis-manager.ts
+++ src/features/cross-chain/cross-chain-symbiosis-manager.ts
@@ -1,10 +1,14 @@
-import { Symbiosis } from 'symbiosis-js-sdk';
+import { ChainId, CHAINS_PRIORITY, Symbiosis, Token as SymbiosisToken } from 'symbiosis-js-sdk';
 import { getSymbiosisConfig } from '@rsdk-features/cross-chain/providers/symbiosis-trade-provider/constants/symbiosis-config';
 import { Injector } from '@rsdk-core/sdk/injector';
 import { PendingRequest } from 'symbiosis-js-sdk/dist/crosschain/pending';
 import BigNumber from 'bignumber.js';
 import { SwapTransactionOptions } from 'src/features';
 import { FailedToCheckForTransactionReceiptError, RubicSdkError } from 'src/common';
+import { BlockchainName, BlockchainsInfo, Token } from 'src/core';
+import { WaitForComplete } from 'symbiosis-js-sdk/dist/crosschain/waitForComplete';
+import { TransactionReceipt as EthersReceipt, Log as EthersLog } from '@ethersproject/providers';
+import { TransactionReceipt } from 'web3-eth';
 
 export class CrossChainSymbiosisManager {
     private readonly symbiosis = new Symbiosis(getSymbiosisConfig(), 'rubic');
@@ -20,6 +24,38 @@ export class CrossChainSymbiosisManager {
         }
 
         return this.symbiosis.getPendingRequests(fromAddress);
+    }
+
+    /**
+     * Waiting for symbiosis trade to complete.
+     * @param fromBlockchain Trade from blockchain.
+     * @param toBlockchain Trade to blockchain.
+     * @param toToken Trade to toke.
+     * @param receipt Transaction receipt.
+     * @returns Promise<EthersLog>
+     */
+    public async waitForComplete(
+        fromBlockchain: BlockchainName,
+        toBlockchain: BlockchainName,
+        toToken: Token,
+        receipt: TransactionReceipt
+    ): Promise<EthersLog> {
+        const fromChainId = BlockchainsInfo.getBlockchainByName(fromBlockchain).id as ChainId;
+        const toChainId = BlockchainsInfo.getBlockchainByName(toBlockchain).id as ChainId;
+        const tokenOut = new SymbiosisToken({
+            chainId: toChainId,
+            address: toToken.isNative ? '' : toToken.address,
+            decimals: toToken.decimals,
+            isNative: toToken.isNative
+        });
+
+        return await new WaitForComplete({
+            direction: this.getDirection(fromChainId, toChainId),
+            symbiosis: this.symbiosis,
+            revertableAddress: this.walletAddress,
+            tokenOut,
+            chainIdIn: fromChainId
+        }).waitForComplete(receipt as unknown as EthersReceipt);
     }
 
     public async revertTrade(
@@ -66,5 +102,12 @@ export class CrossChainSymbiosisManager {
             }
             throw err;
         }
+    }
+
+    private getDirection(chainIdIn: ChainId, chainIdOut: ChainId): 'burn' | 'mint' {
+        const indexIn = CHAINS_PRIORITY.indexOf(chainIdIn);
+        const indexOut = CHAINS_PRIORITY.indexOf(chainIdOut);
+
+        return indexIn > indexOut ? 'burn' : 'mint';
     }
 }
