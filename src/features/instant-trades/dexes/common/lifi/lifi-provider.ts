ab745fa41975bb9fee1607415d4289840b82cd28
ottebrut <ott.andrey.ao@gmail.com>
2022-07-14 16:15:20 +0300

    Add gas fee and path calculation to lifi instant trades

--- src/features/instant-trades/dexes/common/lifi/lifi-provider.ts
+++ src/features/instant-trades/dexes/common/lifi/lifi-provider.ts
@@ -1,7 +1,7 @@
-import { SwapCalculationOptions, TradeType } from 'src/features';
+import { GasFeeInfo, SwapCalculationOptions, TradeType } from 'src/features';
 import { InstantTrade } from 'src/features/instant-trades/instant-trade';
-import { BlockchainsInfo, PriceToken } from 'src/core';
-import LIFI, { RouteOptions, RoutesRequest } from '@lifinance/sdk';
+import { BlockchainName, BlockchainsInfo, PriceToken, Web3Pure } from 'src/core';
+import LIFI, { RouteOptions, RoutesRequest, Step } from '@lifinance/sdk';
 import { PriceTokenAmount } from 'src/core/blockchain/tokens/price-token-amount';
 import { EMPTY_ADDRESS } from 'src/core/blockchain/constants/empty-address';
 import { combineOptions } from 'src/common/utils/options';
@@ -9,6 +9,8 @@ import { lifiProviders } from 'src/features/instant-trades/dexes/common/lifi/con
 import { notNull } from 'src/common';
 import { LifiTrade } from 'src/features/instant-trades/dexes/common/lifi/lifi-trade';
 import BigNumber from 'bignumber.js';
+import { Injector } from 'src/core/sdk/injector';
+import { Token } from 'src/core/blockchain/tokens/token';
 
 export class LifiProvider {
     private readonly lifi = new LIFI();
@@ -64,35 +66,83 @@ export class LifiProvider {
         const result = await this.lifi.getRoutes(routesRequest);
         const { routes } = result;
 
-        return routes
-            .map(route => {
-                const step = route.steps[0];
-                if (!step) {
-                    return null;
-                }
+        return (
+            await Promise.all(
+                routes.map(async route => {
+                    const step = route.steps[0];
+                    if (!step) {
+                        return null;
+                    }
 
-                const to = new PriceTokenAmount({
-                    ...toToken.asStruct,
-                    weiAmount: new BigNumber(route.toAmount)
-                });
+                    const to = new PriceTokenAmount({
+                        ...toToken.asStruct,
+                        weiAmount: new BigNumber(route.toAmount)
+                    });
 
-                const contractAddress = step.estimate.approvalAddress;
-                const type = lifiProviders[step.toolDetails.name.toLowerCase()];
-                if (!type) {
-                    return null;
-                }
+                    const contractAddress = step.estimate.approvalAddress;
+                    const type = lifiProviders[step.toolDetails.name.toLowerCase()];
+                    if (!type) {
+                        return null;
+                    }
 
-                return new LifiTrade({
-                    from,
-                    to,
-                    gasFeeInfo: null,
-                    slippageTolerance: fullOptions.slippageTolerance,
-                    contractAddress,
-                    type,
-                    path: [],
-                    route
-                });
-            })
-            .filter(notNull);
+                    const [gasFeeInfo, path] = await Promise.all([
+                        fullOptions.gasCalculation === 'disabled'
+                            ? null
+                            : this.getGasFeeInfo(step, from.blockchain),
+                        this.getPath(step, from, to)
+                    ]);
+
+                    return new LifiTrade({
+                        from,
+                        to,
+                        gasFeeInfo,
+                        slippageTolerance: fullOptions.slippageTolerance,
+                        contractAddress,
+                        type,
+                        path,
+                        route
+                    });
+                })
+            )
+        ).filter(notNull);
+    }
+
+    private async getGasFeeInfo(
+        step: Step,
+        fromBlockchain: BlockchainName
+    ): Promise<GasFeeInfo | null> {
+        try {
+            const gasLimit = step.estimate.gasCosts?.[0]?.limit;
+            if (!gasLimit) {
+                return null;
+            }
+
+            const [gasPrice, nativeCoinPrice] = await Promise.all([
+                Injector.gasPriceApi.getGasPrice(fromBlockchain),
+                Injector.coingeckoApi.getNativeCoinPrice(fromBlockchain)
+            ]);
+
+            const gasPriceInEth = Web3Pure.fromWei(gasPrice);
+            const gasPriceInUsd = gasPriceInEth.multipliedBy(nativeCoinPrice);
+
+            const gasFeeInEth = gasPriceInEth.multipliedBy(gasLimit);
+            const gasFeeInUsd = gasPriceInUsd.multipliedBy(gasLimit);
+
+            return {
+                gasLimit: new BigNumber(gasLimit),
+                gasPrice: new BigNumber(gasPrice),
+                gasFeeInEth,
+                gasFeeInUsd
+            };
+        } catch (_err) {
+            return null;
+        }
+    }
+
+    private async getPath(step: Step, from: Token, to: Token): Promise<ReadonlyArray<Token>> {
+        const estimatedPath = step.estimate.data.path;
+        return estimatedPath
+            ? await Token.createTokens(estimatedPath, from.blockchain)
+            : [from, to];
     }
 }
