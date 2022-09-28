5e1af2e46d152599b70ce78594ea76c364a579bc
axtezy <axtezy@gmail.com>
2022-09-28 17:31:17 +0300

    Add new contract in Tron

--- src/features/cross-chain/providers/common/constants/rubic-proxy-contract-address.ts
+++ src/features/cross-chain/providers/common/constants/rubic-proxy-contract-address.ts
@@ -4,9 +4,8 @@ export const rubicProxyContractAddress: Record<BlockchainName, string> = Object.
     BLOCKCHAIN_NAME
 ).reduce((acc, blockchain) => {
     let contractAddress = '0x33388CF69e032C6f60A420b37E44b1F5443d3333';
-    // Contract in OKEX can't be verified.
     if (blockchain === BLOCKCHAIN_NAME.TRON) {
-        contractAddress = 'TBzbnMwAJZdxiQmeAYrywa27BT3qQFww99';
+        contractAddress = 'TYfrVeJLCokb4fpK5Fx29BoVfyoBFrW1C4';
     }
     return {
         ...acc,
