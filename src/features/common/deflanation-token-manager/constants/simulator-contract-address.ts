0958b00bcb28439f09169add1c282eb107b9508e
kolebayevv <ko1ebayev.worx@gmail.com>
2022-10-18 12:18:47 +0300

    add deflanation token manager class

---
+++ src/features/common/deflanation-token-manager/constants/simulator-contract-address.ts
@@ -0,0 +1,5 @@
+import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
+
+export const simulatorContractAddress = {
+    [BLOCKCHAIN_NAME.POLYGON]: '0x92CAa8Fbbd9A84a31F5bDbcAe5E5B9fE2bF4b0b5'
+};
