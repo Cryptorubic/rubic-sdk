37c78ed76e8801e9f36b596f29103e694cbfd1f6
ottebrut <ott.andrey.ao@gmail.com>
2022-10-19 16:48:27 +0300

    minor fix

--- src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/models/supported-blockchain.ts
+++ src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/models/supported-blockchain.ts
@@ -1,6 +1,7 @@
 import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 
 export const multichainProxyCrossChainSupportedBlockchains = [
+    BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
     BLOCKCHAIN_NAME.POLYGON,
     BLOCKCHAIN_NAME.KAVA
 ] as const;
