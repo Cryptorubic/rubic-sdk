97d9052870db0c1acd6da493d7b8c5eff8b6e085
ottebrut <ott.andrey.ao@gmail.com>
2022-11-02 02:08:18 +0400

    common fixes

--- src/features/cross-chain/calculation-manager/utils/compare-cross-chain-trades.ts
+++ src/features/cross-chain/calculation-manager/utils/compare-cross-chain-trades.ts
@@ -7,6 +7,28 @@ import { SymbiosisCrossChainTrade } from 'src/features/cross-chain/calculation-m
 import { LifiCrossChainTrade } from 'src/features/cross-chain/calculation-manager/providers/lifi-provider/lifi-cross-chain-trade';
 import { ViaCrossChainTrade } from 'src/features/cross-chain/calculation-manager/providers/via-provider/via-cross-chain-trade';
 import { RangoCrossChainTrade } from 'src/features/cross-chain/calculation-manager/providers/rango-provider/rango-cross-chain-trade';
+import { WrappedCrossChainTrade } from 'src/features/cross-chain/calculation-manager/providers/common/models/wrapped-cross-chain-trade';
+
+function getPrevTradeUsdCost(prevWrappedTrade: WrappedCrossChainTrade): BigNumber {
+    const prevTrade = prevWrappedTrade.trade;
+    if (prevTrade instanceof CelerCrossChainTrade) {
+        return prevTrade.fromTrade.toToken.tokenAmount;
+    }
+    if (
+        prevTrade instanceof DebridgeCrossChainTrade ||
+        prevTrade instanceof SymbiosisCrossChainTrade
+    ) {
+        return prevTrade.transitAmount;
+    }
+    if (
+        prevTrade instanceof LifiCrossChainTrade ||
+        prevTrade instanceof ViaCrossChainTrade ||
+        prevTrade instanceof RangoCrossChainTrade
+    ) {
+        return prevTrade.from.price.multipliedBy(prevTrade.from.tokenAmount);
+    }
+    throw new RubicSdkError('Not supported trade');
+}
 
 /**
  * Compares two cross chain trades for sorting algorithm.
@@ -28,31 +50,21 @@ export function compareCrossChainTrades(
         return prevWrappedTrade.error.maxAmount.gte(nextWrappedTrade.error.maxAmount) ? 1 : -1;
     }
 
+    if (!prevWrappedTrade || prevWrappedTrade.error) {
+        if (
+            nextWrappedTrade?.trade ||
+            nextWrappedTrade?.error instanceof MinAmountError ||
+            nextWrappedTrade?.error instanceof MaxAmountError
+        ) {
+            return -1;
+        }
+        return 1;
+    }
     if (!nextWrappedTrade || nextWrappedTrade.error) {
         return 1;
     }
-    if (!prevWrappedTrade || prevWrappedTrade.error) {
-        return -1;
-    }
 
-    const prevTrade = prevWrappedTrade.trade;
-    let fromUsd: BigNumber;
-    if (prevTrade instanceof CelerCrossChainTrade) {
-        fromUsd = prevTrade.fromTrade.toToken.tokenAmount;
-    } else if (
-        prevTrade instanceof DebridgeCrossChainTrade ||
-        prevTrade instanceof SymbiosisCrossChainTrade
-    ) {
-        fromUsd = prevTrade.transitAmount;
-    } else if (
-        prevTrade instanceof LifiCrossChainTrade ||
-        prevTrade instanceof ViaCrossChainTrade ||
-        prevTrade instanceof RangoCrossChainTrade
-    ) {
-        fromUsd = prevTrade.from.price.multipliedBy(prevTrade.from.tokenAmount);
-    } else {
-        throw new RubicSdkError('Not supported trade');
-    }
+    const fromUsd = getPrevTradeUsdCost(prevWrappedTrade);
 
     const prevTradeRatio = prevWrappedTrade?.trade?.getTradeAmountRatio(fromUsd);
     const nextTradeRatio = nextWrappedTrade?.trade?.getTradeAmountRatio(fromUsd);
