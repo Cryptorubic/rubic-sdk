3952aa06d011144d77521896b3f9b42b1cdc1982
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-09 17:09:33 +0300

    add oasis and metis networks

--- src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/constants/contract-address.ts
+++ src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/constants/contract-address.ts
@@ -7,8 +7,21 @@ export const multichainProxyContractAddress: Record<
     MultichainProxyCrossChainSupportedBlockchain,
     string
 > = multichainProxyCrossChainSupportedBlockchains.reduce((acc, blockchain) => {
+    let address;
+
+    switch (blockchain) {
+        case 'METIS':
+            address = '0x333BE852042F435431967664e09315CC63593333';
+            break;
+        case 'OASIS':
+            address = '0x333BE852042F435431967664e09315CC63593333';
+            break;
+        default:
+            address = '0x333b8881485fB8dE9af05d0B259a7f3f032B3333';
+    }
+
     return {
         ...acc,
-        [blockchain]: '0x333b8881485fB8dE9af05d0B259a7f3f032B3333'
+        [blockchain]: address
     };
 }, {} as Record<MultichainProxyCrossChainSupportedBlockchain, string>);
