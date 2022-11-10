fbcf63c3607c0c9b54d0a4a588704b95279c9824
kolebayevv <ko1ebayev.worx@gmail.com>
2022-11-10 16:15:04 +0300

    update barrel

--- src/common/tokens/index.ts
+++ src/common/tokens/index.ts
@@ -1,3 +1,5 @@
 export { Token } from './token';
 export { PriceToken } from './price-token';
 export { PriceTokenAmount } from './price-token-amount';
+export { wrappedNativeTokensList } from './constants/wrapped-native-tokens';
+export { nativeTokensList } from './constants/native-tokens';
