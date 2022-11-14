f01129d0cf0bf10d4e447507822890a9bb5b2e4b
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-14 11:28:59 +0300

    fix multichain proxy contract address

--- src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/constants/contract-address.ts
+++ src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/constants/contract-address.ts
@@ -7,21 +7,8 @@ export const multichainProxyContractAddress: Record<
     MultichainProxyCrossChainSupportedBlockchain,
     string
 > = multichainProxyCrossChainSupportedBlockchains.reduce((acc, blockchain) => {
-    let address;
-
-    switch (blockchain) {
-        case 'METIS':
-            address = '0x333BE852042F435431967664e09315CC63593333';
-            break;
-        case 'OASIS':
-            address = '0x333BE852042F435431967664e09315CC63593333';
-            break;
-        default:
-            address = '0x333b8881485fB8dE9af05d0B259a7f3f032B3333';
-    }
-
     return {
         ...acc,
-        [blockchain]: address
+        [blockchain]: '0x333BE852042F435431967664e09315CC63593333'
     };
 }, {} as Record<MultichainProxyCrossChainSupportedBlockchain, string>);
