f946e2c6035e3c126833704ac2ee9951645c7484
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-16 11:19:47 +0300

    add elk and surfdex

---
+++ src/features/on-chain/calculation-manager/providers/dexes/kava/elk/constants.ts
@@ -0,0 +1,5 @@
+import { defaultKavaProviderConfiguration } from 'src/features/on-chain/calculation-manager/providers/dexes/kava/default-constants';
+
+export const ELK_CONTRACT_ADDRESS = '0x7a2c1D96C76B6EB62241df4d2fAEb9F0D3D59E10';
+
+export const ELK_PROVIDER_CONFIGURATION = defaultKavaProviderConfiguration;
