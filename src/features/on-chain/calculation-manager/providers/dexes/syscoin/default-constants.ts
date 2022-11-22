82d2b440f36024f40ba939fd52817261ecabf9da
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-22 13:54:21 +0300

    add abi for pegasys dex

--- src/features/on-chain/calculation-manager/providers/dexes/syscoin/default-constants.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/syscoin/default-constants.ts
@@ -2,16 +2,16 @@ import { UniswapV2ProviderConfiguration } from 'src/features/on-chain/calculatio
 import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 import { wrappedNativeTokensList } from 'src/common/tokens';
 
-const defaultPegasysRoutingProvidersAddresses = [
+const defaultSyscoinRoutingProvidersAddresses = [
     wrappedNativeTokensList[BLOCKCHAIN_NAME.SYSCOIN].address, // WSYS
     '0x2bF9b864cdc97b08B6D79ad4663e71B8aB65c45c', // USDC
     '0x922D641a426DcFFaeF11680e5358F34d97d112E1' // USDT
 ];
 
-const defaultPegasysWethAddress = wrappedNativeTokensList[BLOCKCHAIN_NAME.SYSCOIN].address;
+const defaultSyscoinWethAddress = wrappedNativeTokensList[BLOCKCHAIN_NAME.SYSCOIN].address;
 
 export const defaultSyscoinProviderConfiguration: UniswapV2ProviderConfiguration = {
     maxTransitTokens: 2,
-    routingProvidersAddresses: defaultPegasysRoutingProvidersAddresses,
-    wethAddress: defaultPegasysWethAddress
+    routingProvidersAddresses: defaultSyscoinRoutingProvidersAddresses,
+    wethAddress: defaultSyscoinWethAddress
 };
--- src/features/on-chain/calculation-manager/providers/dexes/syscoin/pegasys/pegasys-trade.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/syscoin/pegasys/pegasys-trade.ts
@@ -7,8 +7,14 @@ import {
     OnChainTradeType
 } from 'src/features/on-chain/calculation-manager/providers/models/on-chain-trade-type';
 import { PEGASYS_CONTRACT_ADDRESS } from 'src/features/on-chain/calculation-manager/providers/dexes/syscoin/pegasys/constants';
+import { SYS_SWAP_METHOD } from 'src/features/on-chain/calculation-manager/providers/dexes/syscoin/syscoin-swap-method';
+import { SYS_ABI } from 'src/features/on-chain/calculation-manager/providers/dexes/syscoin/syscoin-abi';
 
 export class PegasysTrade extends UniswapV2AbstractTrade {
+    public static readonly contractAbi = SYS_ABI;
+
+    public static readonly swapMethods = SYS_SWAP_METHOD;
+
     public static get type(): OnChainTradeType {
         return ON_CHAIN_TRADE_TYPE.PEGASYS;
     }
