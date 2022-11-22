11e785c69eb7f8eed9fa3affe35b05608d65e6e5
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-22 14:37:59 +0300

    upd contract address for pegasys dex

--- src/features/on-chain/calculation-manager/providers/dexes/syscoin/pegasys/constants.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/syscoin/pegasys/constants.ts
@@ -1,5 +1,5 @@
 import { defaultSyscoinProviderConfiguration } from 'src/features/on-chain/calculation-manager/providers/dexes/syscoin/default-constants';
 
-export const PEGASYS_CONTRACT_ADDRESS = '0x78B4733FEF7Ee3a5e233D9D7840ac7Af174CA2Ad';
+export const PEGASYS_CONTRACT_ADDRESS = '0x017dAd2578372CAEE5c6CddfE35eEDB3728544C4';
 
 export const PEGASYS_PROVIDER_CONFIGURATION = defaultSyscoinProviderConfiguration;
