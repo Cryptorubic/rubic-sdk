f847a3ff55fd4071f6d5544cc7f80fe846283b2f
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-23 16:33:26 +0300

    minor fix

--- src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/dex-multichain-cross-chain-trade.ts
+++ src/features/cross-chain/calculation-manager/providers/multichain-provider/dex-multichain-provider/dex-multichain-cross-chain-trade.ts
@@ -152,7 +152,7 @@ export class DexMultichainCrossChainTrade extends MultichainCrossChainTrade {
             const encodedData = (
                 await this.onChainTrade.encode({
                     fromAddress: options.fromAddress || this.walletAddress,
-                    receiverAddress: this.fromContractAddress,
+                    receiverAddress,
                     supportFee: false
                 })
             ).data;
