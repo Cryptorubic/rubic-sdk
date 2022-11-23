52b8e7362300a4d4df03ef8fcebbf14608dd4343
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-23 12:08:39 +0300

    add abi for klaytn network

--- src/features/on-chain/calculation-manager/providers/dexes/klaytn/claim-swap/claim-swap-trade.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/klaytn/claim-swap/claim-swap-trade.ts
@@ -7,8 +7,14 @@ import {
     OnChainTradeType
 } from 'src/features/on-chain/calculation-manager/providers/models/on-chain-trade-type';
 import { CLAIM_SWAP_CONTRACT_ADDRESS } from 'src/features/on-chain/calculation-manager/providers/dexes/klaytn/claim-swap/constants';
+import { KLAY_SWAP_METHOD } from 'src/features/on-chain/calculation-manager/providers/dexes/klaytn/klaytn-swap-method';
+import { KLAY_ABI } from 'src/features/on-chain/calculation-manager/providers/dexes/klaytn/klaytn-abi';
 
 export class ClaimSwapTrade extends UniswapV2AbstractTrade {
+    public static readonly contractAbi = KLAY_ABI;
+
+    public static readonly swapMethods = KLAY_SWAP_METHOD;
+
     public static get type(): OnChainTradeType {
         return ON_CHAIN_TRADE_TYPE.CLAIM_SWAP;
     }
