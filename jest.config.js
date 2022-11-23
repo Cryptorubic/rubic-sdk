e68ebbd825364170d67d6d097ce5864c687f56b1
kolebayevv <ko1ebayev.worx@gmail.com>
2022-11-23 17:31:14 +0300

    fix config typos

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   roots: [
     "src", "__tests__"
   ],
-  moduleDirectories: ['node_modules', '<root-dir>/src'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'node',
   transform: {
     "node_modules/(ethereum-cryptography)/.+\\.(j|t)sx?$": "ts-jest",
@@ -12,12 +12,12 @@ module.exports = {
   testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.test.json'
+      tsConfig: 'tsconfig.test.json'
     }
   },
   moduleNameMapper: {
-    '^src/(.*)$': '<root-dir>/../../src/$1',
-    '__tests__/(.*)$': '<root-dir>/../../__tests__/$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '__tests__/(.*)$': '<rootDir>/__tests__/$1',
   },
   testTimeout: 300_000
 };
