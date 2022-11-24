43766f77b784ddd4cf57ec5a310bf5f24fb0392e
Kirill Demidov <kirill.demidov.1995@mail.ru>
2022-11-24 13:19:00 +0300

    minor fix

--- src/features/on-chain/calculation-manager/providers/lifi/lifi-provider.ts
+++ src/features/on-chain/calculation-manager/providers/lifi/lifi-provider.ts
@@ -30,8 +30,6 @@ export class LifiProvider {
 
     private readonly onChainProxyService = new OnChainProxyService();
 
-    public readonly supportReceiverAddress = false;
-
     private readonly defaultOptions: Omit<RequiredLifiCalculationOptions, 'disabledProviders'> = {
         ...evmProviderDefaultOptions,
         gasCalculation: 'calculate'
