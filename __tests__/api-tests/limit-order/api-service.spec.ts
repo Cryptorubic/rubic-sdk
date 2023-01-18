83f672f0a6b456e405339ed1e800c5afe7155e1d
ottebrut <ott.andrey.ao@gmail.com>
2023-01-18 18:01:43 +0400

    update api service tests

--- __tests__/api-tests/limit-order/api-service.spec.ts
+++ __tests__/api-tests/limit-order/api-service.spec.ts
@@ -1,9 +1,14 @@
 import { LimitOrderApiService } from 'src/features/limit-order/limit-order-api-service';
 import { blockchainId } from 'src/core/blockchain/utils/blockchains-info/constants/blockchain-id';
 import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
-import { orderHash, userData } from '__tests__/api-tests/limit-order/constants/user-data';
+import {
+    orderApi,
+    orderParsed,
+    userAddress
+} from '__tests__/api-tests/limit-order/constants/user-data';
 import { SDK } from 'src/core/sdk/sdk';
 import { sdkConfiguration } from '__tests__/api-tests/limit-order/constants/sdk-configuration';
+import { Any } from 'src/common/utils/types';
 
 describe('Limit Order Api Service integration tests', () => {
     let apiService: LimitOrderApiService;
@@ -16,17 +21,17 @@ describe('Limit Order Api Service integration tests', () => {
     test('Get not empty array of polygon orders', async () => {
         const orders = await apiService['getApiOrders'](
             blockchainId[BLOCKCHAIN_NAME.POLYGON],
-            userData
+            userAddress
         );
         expect(orders.length).toBeGreaterThan(0);
-        const order = orders.find(v => v.orderHash === orderHash);
-        expect(order).toBeDefined();
+        const order = orders.find(v => v.orderHash === orderApi.orderHash);
+        expect(order).toMatchObject(orderApi);
     });
 
     test('Get empty array of ethereum orders', async () => {
         const orders = await apiService['getApiOrders'](
             blockchainId[BLOCKCHAIN_NAME.ETHEREUM],
-            userData
+            userAddress
         );
         expect(orders.length).toBe(0);
     });
@@ -34,10 +39,68 @@ describe('Limit Order Api Service integration tests', () => {
     test('Catch error on getting orders with invalid parameters', async () => {
         let err: Error | undefined;
         try {
-            await apiService['getApiOrders'](-1, userData);
+            await apiService['getApiOrders'](-1, userAddress);
         } catch (apiErr) {
             err = apiErr;
         }
         expect(err).toBeDefined();
+    });
+
+    test('Parse api limit order', async () => {
+        const limitOrder = await apiService['parseLimitOrder'](BLOCKCHAIN_NAME.POLYGON, orderApi);
+        expect(limitOrder).toEqual(orderParsed);
+    });
+
+    test('Get order by hash', async () => {
+        const getApiOrdersFn = jest.spyOn(apiService as Any, 'getApiOrders');
+        const order = await apiService.getOrderByHash(
+            userAddress,
+            BLOCKCHAIN_NAME.POLYGON,
+            orderApi.orderHash
+        );
+        expect(getApiOrdersFn).toBeCalled();
+        expect(order).toMatchObject(orderApi);
+    });
+
+    test('Get null by invalid order hash', async () => {
+        const order = await apiService.getOrderByHash(
+            userAddress,
+            BLOCKCHAIN_NAME.POLYGON,
+            `${orderApi.orderHash}_invalid`
+        );
+        expect(order).toBeNull();
+    });
+
+    test('Get null, because user does not have order', async () => {
+        const order = await apiService.getOrderByHash(
+            `${userAddress}_invalid`,
+            BLOCKCHAIN_NAME.POLYGON,
+            orderApi.orderHash
+        );
+        expect(order).toBeNull();
+    });
+
+    test('Get not empty array of user orders', async () => {
+        const getApiOrdersFn = jest.spyOn(apiService as Any, 'getApiOrders');
+        const parseLimitOrderFn = jest.spyOn(apiService as Any, 'parseLimitOrder');
+        const orders = await apiService.getUserOrders(userAddress);
+
+        expect(getApiOrdersFn).toBeCalled();
+        expect(parseLimitOrderFn).toBeCalled();
+
+        expect(orders.length).toBeGreaterThan(0);
+        const order = orders.find(v => v.hash === orderParsed.hash);
+        expect(order).toEqual(orderParsed);
+    });
+
+    test('Get empty array of invalid user orders', async () => {
+        const getApiOrdersFn = jest.spyOn(apiService as Any, 'getApiOrders');
+        const parseLimitOrderFn = jest.spyOn(apiService as Any, 'parseLimitOrder');
+        const orders = await apiService.getUserOrders(`${userAddress}_invalid`);
+
+        expect(getApiOrdersFn).toBeCalled();
+        expect(parseLimitOrderFn).not.toBeCalled();
+
+        expect(orders.length).toBe(0);
     });
 });
--- __tests__/api-tests/limit-order/constants/user-data.ts
+++ __tests__/api-tests/limit-order/constants/user-data.ts
@@ -1,9 +1,47 @@
+import { LimitOrderApi } from 'src/features/limit-order/models/limit-order-api';
+import { TOKENS } from '__tests__/utils/tokens';
+import { LIMIT_ORDER_STATUS } from 'src/features/limit-order/models/limit-order-status';
+import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
+import { LimitOrder } from 'src/features/limit-order/models/limit-order';
+import BigNumber from 'bignumber.js';
+
 /**
  * Wallet address, which always has not expired limit orders in 1inch api.
  */
-export const userData = '0x11738D36B166C4F18028fE2fa28050fdbd91c430';
+export const userAddress = '0x11738D36B166C4F18028fE2fa28050fdbd91c430';
 
 /**
- * Order hash, which will never be expired.
+ * Order, which will never be expired.
  */
-export const orderHash = '0x87aa138bb4e6d78d6efc01ee8a03d66fd127c50d48163ad2b4fe8c46bd9bde58';
+export const orderApi: LimitOrderApi = {
+    orderHash: '0x87aa138bb4e6d78d6efc01ee8a03d66fd127c50d48163ad2b4fe8c46bd9bde58',
+    createDateTime: '2023-01-17T15:58:27.930Z',
+    data: {
+        makerAsset: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+        takerAsset: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+        salt: '869234911695',
+        receiver: '0x0000000000000000000000000000000000000000',
+        allowedSender: '0x0000000000000000000000000000000000000000',
+        makingAmount: '1000000',
+        takingAmount: '10000000',
+        maker: '0x11738d36b166c4f18028fe2fa28050fdbd91c430',
+        interactions:
+            '0xbf15fcd8000000000000000000000000a5eb255ef45dfb48b5d133d08833def69871691d000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000242cc2878d050bde8da00000000000000011738d36b166c4f18028fe2fa28050fdbd91c43000000000000000000000000000000000000000000000000000000000',
+        offsets: '4421431254442149611168492388118363282642987198110904030635476664713216'
+    },
+    orderInvalidReason: null
+};
+
+/**
+ * Parsed {@link orderApi}.
+ */
+export const orderParsed: LimitOrder = {
+    hash: '0x87aa138bb4e6d78d6efc01ee8a03d66fd127c50d48163ad2b4fe8c46bd9bde58',
+    creation: new Date('2023-01-17T15:58:27.930Z'),
+    fromToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDC,
+    toToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDT,
+    fromAmount: new BigNumber(1),
+    toAmount: new BigNumber(10),
+    expiration: new Date('2656-10-27T03:31:44.000Z'),
+    status: LIMIT_ORDER_STATUS.VALID
+};
