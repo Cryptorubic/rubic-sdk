8bc312bd0a4305d452067ba16542b3cdc3f7b483
ottebrut <ott.andrey.ao@gmail.com>
2023-02-06 14:48:11 +0400

    bug fixes

--- __tests__/unit-tests/features/limit-order/constants/limit-orders.ts
+++ __tests__/unit-tests/features/limit-order/constants/limit-orders.ts
@@ -8,41 +8,49 @@ export const orders: LimitOrder[] = [
     {
         hash: '0x7c3923040b40aa6205a4dc6986e5dd8930e7507732a612fb2d532fa4fb14e198',
         creation: new Date('2023-01-12T13:50:31.042Z'),
+        expiration: new Date('2023-01-12T15:58:27.930Z'),
         fromToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDC,
         toToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].DAI,
         fromAmount: new BigNumber(1),
         toAmount: new BigNumber(1),
-        expiration: new Date('2023-01-12T15:58:27.930Z'),
-        status: LIMIT_ORDER_STATUS.VALID
+        fromBalance: new BigNumber(0),
+        status: LIMIT_ORDER_STATUS.VALID,
+        filledPercent: 0
     },
     {
         hash: '0x87aa138bb4e6d78d6efc01ee8a03d66fd127c50d48163ad2b4fe8c46bd9bde58',
         creation: new Date('2023-01-17T15:58:27.930Z'),
+        expiration: new Date('2023-01-24T15:58:27.930Z'),
         fromToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDC,
         toToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDT,
         fromAmount: new BigNumber(1),
         toAmount: new BigNumber(1),
-        expiration: new Date('2023-01-24T15:58:27.930Z'),
-        status: LIMIT_ORDER_STATUS.VALID
+        fromBalance: new BigNumber(0),
+        status: LIMIT_ORDER_STATUS.VALID,
+        filledPercent: 0
     },
     {
         hash: '0x9e0b2ea33074942ce788a26a5045bcdbdeb33d04fc28181fef6b3d55499a66c0',
         creation: new Date('2023-01-13T10:44:42.329Z'),
+        expiration: new Date('2023-01-24T15:58:27.930Z'),
         fromToken: TOKENS[BLOCKCHAIN_NAME.ETHEREUM].ETH,
         toToken: TOKENS[BLOCKCHAIN_NAME.ETHEREUM].USDT,
         fromAmount: new BigNumber(1),
         toAmount: new BigNumber(1),
-        expiration: new Date('2023-01-24T15:58:27.930Z'),
-        status: LIMIT_ORDER_STATUS.VALID
+        fromBalance: new BigNumber(0),
+        status: LIMIT_ORDER_STATUS.VALID,
+        filledPercent: 0
     },
     {
         hash: '0x9f5885b2a9aab432c861c90468fa0917aafa60373e9b94c3bf0927b190117b3d',
         creation: new Date('2022-01-17T15:58:27.930Z'),
+        expiration: new Date('2022-01-24T15:58:27.930Z'),
         fromToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].USDC,
         toToken: TOKENS[BLOCKCHAIN_NAME.POLYGON].WMATIC,
         fromAmount: new BigNumber(1),
         toAmount: new BigNumber(1),
-        expiration: new Date('2022-01-24T15:58:27.930Z'),
-        status: LIMIT_ORDER_STATUS.VALID
+        fromBalance: new BigNumber(0),
+        status: LIMIT_ORDER_STATUS.VALID,
+        filledPercent: 0
     }
 ];
