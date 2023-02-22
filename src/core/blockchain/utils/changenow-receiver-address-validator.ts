1bc99fa23f9cf35b18e6e5c616b9b965aeb7d369
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-02-22 18:43:01 +0300

    upd validation

---
+++ src/core/blockchain/utils/changenow-receiver-address-validator.ts
@@ -0,0 +1,24 @@
+import { Injector } from 'src/core/injector/injector';
+import { nonEvmChainAddressCorrectResponse } from 'src/features/common/models/non-evm-chain-address-correct-response';
+
+export const isChangenowReceiverAddressCorrect = async (
+    address: string,
+    chain: string,
+    regEx: RegExp
+): Promise<boolean> => {
+    try {
+        const response = await Injector.httpClient.get<nonEvmChainAddressCorrectResponse>(
+            `https://api.changenow.io/v2/validate/address?currency=${chain}&address=${address}`
+        );
+
+        return response.result;
+    } catch (error) {
+        if (address === null) {
+            return false;
+        }
+
+        const isCorrect = address.match(regEx);
+
+        return !!(isCorrect !== null && isCorrect[0]);
+    }
+};
