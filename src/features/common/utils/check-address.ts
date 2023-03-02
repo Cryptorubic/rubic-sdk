23048d89d466443c45f316f375c0cdbdcd061956
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-03-02 12:03:55 +0300

    fix changenow trade validation

--- src/features/common/utils/check-address.ts
+++ src/features/common/utils/check-address.ts
@@ -7,6 +7,10 @@ import {
     CROSS_CHAIN_TRADE_TYPE,
     CrossChainTradeType
 } from 'src/features/cross-chain/calculation-manager/models/cross-chain-trade-type';
+import {
+    changenowApiBlockchain,
+    ChangenowCrossChainSupportedBlockchain
+} from 'src/features/cross-chain/calculation-manager/providers/changenow-provider/constants/changenow-api-blockchain';
 import { OnChainTradeType } from 'src/features/on-chain/calculation-manager/providers/common/models/on-chain-trade-type';
 
 export async function isAddressCorrect(
@@ -16,8 +20,10 @@ export async function isAddressCorrect(
 ): Promise<boolean> {
     try {
         if (crossChainType === CROSS_CHAIN_TRADE_TYPE.CHANGENOW) {
+            const chain =
+                changenowApiBlockchain[toBlockchain as ChangenowCrossChainSupportedBlockchain];
             const response = await Injector.httpClient.get<nonEvmChainAddressCorrectResponse>(
-                `https://api.changenow.io/v2/validate/address?currency=${toBlockchain.toLowerCase()}&address=${address}`
+                `https://api.changenow.io/v2/validate/address?currency=${chain.toLowerCase()}&address=${address}`
             );
             return response.result;
         }
