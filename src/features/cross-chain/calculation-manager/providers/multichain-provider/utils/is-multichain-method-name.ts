050ece56a0f8e67007105c13d24cd490b2d6e4c5
axtezy <axtezy@gmail.com>
2023-04-06 14:05:33 +0200

    Fix multichain swapout method

--- src/features/cross-chain/calculation-manager/providers/multichain-provider/utils/is-multichain-method-name.ts
+++
@@ -1,8 +0,0 @@
-import {
-    MultichainMethodName,
-    multichainMethodNames
-} from 'src/features/cross-chain/calculation-manager/providers/multichain-provider/models/multichain-method-name';
-
-export function isMultichainMethodName(methodName: string): methodName is MultichainMethodName {
-    return multichainMethodNames.some(multichainMethodName => multichainMethodName === methodName);
-}
