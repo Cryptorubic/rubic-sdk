f021fb76150b205696d8b7ced9efcb6c4ed0270d
axtezy <axtezy@gmail.com>
2023-04-25 14:20:27 +0200

    Remove BUSD

--- src/features/cross-chain/calculation-manager/providers/cbridge/cbridge-cross-chain-api-service.ts
+++ src/features/cross-chain/calculation-manager/providers/cbridge/cbridge-cross-chain-api-service.ts
@@ -1,5 +1,4 @@
 import { Injector } from 'src/core/injector/injector';
-import { CbridgeChainTokenInfo } from 'src/features/cross-chain/calculation-manager/providers/cbridge/models/cbridge-chain-token-info';
 import { CbridgeEstimateAmountRequest } from 'src/features/cross-chain/calculation-manager/providers/cbridge/models/cbridge-estimate-amount-request';
 import { CbridgeEstimateAmountResponse } from 'src/features/cross-chain/calculation-manager/providers/cbridge/models/cbridge-estimate-amount-response';
 import { CbridgeStatusResponse } from 'src/features/cross-chain/calculation-manager/providers/cbridge/models/cbridge-status-response';
@@ -9,41 +8,9 @@ export class CbridgeCrossChainApiService {
     public static readonly apiEndpoint = 'https://cbridge-prod2.celer.app/v2/';
 
     public static async getTransferConfigs(): Promise<CbridgeTransferConfigsResponse> {
-        return Injector.httpClient
-            .get<CbridgeTransferConfigsResponse>(
-                `${CbridgeCrossChainApiService.apiEndpoint}getTransferConfigs`
-            )
-            .then(config => {
-                return {
-                    ...config,
-                    chain_token: {
-                        ...config.chain_token,
-                        592: {
-                            token: [
-                                ...(config.chain_token[592] as CbridgeChainTokenInfo).token,
-                                {
-                                    delay_period: 0,
-                                    delay_threshold: '',
-                                    icon: 'https://get.celer.app/cbridge-icons/BUSD.png',
-                                    inbound_epoch_cap: '',
-                                    inbound_lmt: '',
-                                    liq_add_disabled: false,
-                                    liq_agg_rm_src_disabled: false,
-                                    liq_rm_disabled: false,
-                                    name: 'BUSD',
-                                    token: {
-                                        address: '0x4Bf769b05E832FCdc9053fFFBC78Ca889aCb5E1E',
-                                        decimal: 18,
-                                        symbol: 'BUSD',
-                                        xfer_disabled: false
-                                    },
-                                    transfer_disabled: false
-                                }
-                            ]
-                        }
-                    }
-                };
-            });
+        return Injector.httpClient.get<CbridgeTransferConfigsResponse>(
+            `${CbridgeCrossChainApiService.apiEndpoint}getTransferConfigs`
+        );
     }
 
     public static async fetchEstimateAmount(
