bfc34ff6dcf99eba8bece6f138bd644f3c4242fa
axtezy <axtezy@gmail.com>
2023-07-11 13:22:22 +0200

    Add additional networks

--- src/features/cross-chain/calculation-manager/providers/squidrouter-provider/constants/squidrouter-cross-chain-supported-blockchain.ts
+++ src/features/cross-chain/calculation-manager/providers/squidrouter-provider/constants/squidrouter-cross-chain-supported-blockchain.ts
@@ -3,12 +3,14 @@ import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 export const squidrouterCrossChainSupportedBlockchains = [
     BLOCKCHAIN_NAME.ETHEREUM,
     BLOCKCHAIN_NAME.ARBITRUM,
+    BLOCKCHAIN_NAME.OPTIMISM,
     BLOCKCHAIN_NAME.AVALANCHE,
     BLOCKCHAIN_NAME.POLYGON,
     BLOCKCHAIN_NAME.MOONBEAM,
     BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
     BLOCKCHAIN_NAME.FANTOM,
-    BLOCKCHAIN_NAME.CELO
+    BLOCKCHAIN_NAME.CELO,
+    BLOCKCHAIN_NAME.KAVA
 ] as const;
 
 export type SquidrouterCrossChainSupportedBlockchain =
