d696114df2f60449e93be22c66a0c82f9f2be2d7
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-07-13 18:22:56 +0300

    add proxy

--- src/features/cross-chain/calculation-manager/providers/common/constants/rubic-proxy-contract-address.ts
+++ src/features/cross-chain/calculation-manager/providers/common/constants/rubic-proxy-contract-address.ts
@@ -13,6 +13,9 @@ export const rubicProxyContractAddress: Record<
         if (blockchain === BLOCKCHAIN_NAME.TELOS) {
             router = '0xA2d8CF32C16f070702c45a5686Fdb0a1d7171AAD';
         }
+        if (blockchain === BLOCKCHAIN_NAME.POLYGON_ZKEVM) {
+            router = '0x6AA981bFF95eDfea36Bdae98C26B274FfcafE8d3';
+        }
         if (blockchain === BLOCKCHAIN_NAME.ZK_SYNC) {
             router = '0xa63c029612ddaD00A269383Ab016D1e7c14E851D';
             gateway = '0x8E70e517057e7380587Ea6990dAe81cB1Ba405ce';
