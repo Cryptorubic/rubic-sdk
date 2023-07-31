0672662a8b46b89d7bef9317b4699079e78009ff
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-07-31 14:04:40 +0300

    enabled syncswap and disabled curve

--- src/features/on-chain/calculation-manager/constants/trade-providers/typed-trade-providers.ts
+++ src/features/on-chain/calculation-manager/constants/trade-providers/typed-trade-providers.ts
@@ -2,12 +2,12 @@ import { Mutable } from 'src/common/utils/types';
 import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 import { AlgebraTradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/algebra-trade-providers';
 import { BridgersTradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/bridgers-trade-providers';
-import { CurveTradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/curve-trade-providers';
 import { OneinchTradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/oneinch-trade-providers';
 import { pancakeRouterProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/pancake-router-providers';
 import { UniswapV2TradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/uniswap-v2-trade-providers';
 import { UniswapV3TradeProviders } from 'src/features/on-chain/calculation-manager/constants/trade-providers/uniswap-v3-trade-providers';
 import { OnChainTypedTradeProviders } from 'src/features/on-chain/calculation-manager/models/on-chain-typed-trade-provider';
+import { SyncSwapProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/zksync/sync-swap/sync-swap-provider';
 
 export const typedTradeProviders: OnChainTypedTradeProviders = [
     ...UniswapV2TradeProviders,
@@ -15,9 +15,9 @@ export const typedTradeProviders: OnChainTypedTradeProviders = [
     ...OneinchTradeProviders,
     ...AlgebraTradeProviders,
     ...BridgersTradeProviders,
-    ...CurveTradeProviders,
-    ...pancakeRouterProviders
-    // SyncSwapProvider Removed because of possible hack
+    // ...CurveTradeProviders, Removed because hack
+    ...pancakeRouterProviders,
+    SyncSwapProvider
 ].reduce(
     (acc, ProviderClass) => {
         const provider = new ProviderClass();
