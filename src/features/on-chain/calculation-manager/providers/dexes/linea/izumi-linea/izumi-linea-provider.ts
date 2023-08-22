27b987bb9827fc41113b1b850fbb3f7ce85ab307
axtezy <axtezy@gmail.com>
2023-08-22 16:58:49 +0200

    Fix naming

--- src/features/on-chain/calculation-manager/providers/dexes/linea/izumi-linea/izumi-linea-provider.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/linea/izumi-linea/izumi-linea-provider.ts
@@ -13,8 +13,8 @@ export class IzumiLineaProvider extends IzumiProvider {
         liquidityManagerAddress: '0x1CB60033F61e4fc171c963f0d2d3F63Ece24319c',
         routingTokenAddresses: [
             '0x176211869ca2b568f2a7d4ee941e073a821ee1ff', // USDC
-            wrappedNativeTokensList[BLOCKCHAIN_NAME.LINEA]!.address, // WBNB
-            '0x7d43aabc515c356145049227cee54b608342c0ad',
+            wrappedNativeTokensList[BLOCKCHAIN_NAME.LINEA]!.address, // WETH
+            '0x7d43aabc515c356145049227cee54b608342c0ad', // BUSD
             '0x0a3bb08b3a15a19b4de82f8acfc862606fb69a2d' // iUSD
         ],
         multicallAddress: '0x7a524c7e82874226F0b51aade60A1BE4D430Cf0F',
