ee9e87f7247aaaf453e97961434ac645859fb017
lilchizh <xlilchizhx@gmail.com>
2023-09-01 05:19:44 +0300

    Cut ABIs

--- src/features/on-chain/calculation-manager/providers/dexes/arthera-testnet/algebra-integral/utils/quoter-controller/constants/quoter-contract-data.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/arthera-testnet/algebra-integral/utils/quoter-controller/constants/quoter-contract-data.ts
@@ -5,55 +5,11 @@ export const ALGEBRA_INTEGRAL_QUOTER_CONTRACT_ADDRESS = '0x03f8B4b140249Dc7B2503
 export const ALGEBRA_INTEGRAL_QUOTER_CONTRACT_ABI = [
     {
         inputs: [
-            { internalType: 'address', name: '_factory', type: 'address' },
-            { internalType: 'address', name: '_WNativeToken', type: 'address' },
-            { internalType: 'address', name: '_poolDeployer', type: 'address' }
-        ],
-        stateMutability: 'nonpayable',
-        type: 'constructor'
-    },
-    {
-        inputs: [],
-        name: 'WNativeToken',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [
-            { internalType: 'int256', name: 'amount0Delta', type: 'int256' },
-            { internalType: 'int256', name: 'amount1Delta', type: 'int256' },
-            { internalType: 'bytes', name: 'path', type: 'bytes' }
-        ],
-        name: 'algebraSwapCallback',
-        outputs: [],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [],
-        name: 'factory',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [],
-        name: 'poolDeployer',
-        outputs: [{ internalType: 'address', name: '', type: 'address' }],
-        stateMutability: 'view',
-        type: 'function'
-    },
-    {
-        inputs: [
             { internalType: 'bytes', name: 'path', type: 'bytes' },
             { internalType: 'uint256', name: 'amountIn', type: 'uint256' }
         ],
         name: 'quoteExactInput',
-        outputs: [
-            { internalType: 'uint256', name: 'amountOut', type: 'uint256' },
-            { internalType: 'uint16[]', name: 'fees', type: 'uint16[]' }
-        ],
+        outputs: [{ internalType: 'uint256', name: 'amountOut', type: 'uint256' }],
         stateMutability: 'nonpayable',
         type: 'function'
     },
@@ -65,10 +21,7 @@ export const ALGEBRA_INTEGRAL_QUOTER_CONTRACT_ABI = [
             { internalType: 'uint160', name: 'limitSqrtPrice', type: 'uint160' }
         ],
         name: 'quoteExactInputSingle',
-        outputs: [
-            { internalType: 'uint256', name: 'amountOut', type: 'uint256' },
-            { internalType: 'uint16', name: 'fee', type: 'uint16' }
-        ],
+        outputs: [{ internalType: 'uint256', name: 'amountOut', type: 'uint256' }],
         stateMutability: 'nonpayable',
         type: 'function'
     },
@@ -78,10 +31,7 @@ export const ALGEBRA_INTEGRAL_QUOTER_CONTRACT_ABI = [
             { internalType: 'uint256', name: 'amountOut', type: 'uint256' }
         ],
         name: 'quoteExactOutput',
-        outputs: [
-            { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
-            { internalType: 'uint16[]', name: 'fees', type: 'uint16[]' }
-        ],
+        outputs: [{ internalType: 'uint256', name: 'amountIn', type: 'uint256' }],
         stateMutability: 'nonpayable',
         type: 'function'
     },
@@ -93,10 +43,7 @@ export const ALGEBRA_INTEGRAL_QUOTER_CONTRACT_ABI = [
             { internalType: 'uint160', name: 'limitSqrtPrice', type: 'uint160' }
         ],
         name: 'quoteExactOutputSingle',
-        outputs: [
-            { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
-            { internalType: 'uint16', name: 'fee', type: 'uint16' }
-        ],
+        outputs: [{ internalType: 'uint256', name: 'amountIn', type: 'uint256' }],
         stateMutability: 'nonpayable',
         type: 'function'
     }
