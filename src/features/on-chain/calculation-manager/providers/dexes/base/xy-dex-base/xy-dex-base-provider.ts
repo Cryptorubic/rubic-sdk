c03116b76c037a6eec33e143c7a78b9f8245e209
axtezy <axtezy@gmail.com>
2023-09-04 16:02:49 +0200

    Fix BASE XY dex

--- src/features/on-chain/calculation-manager/providers/dexes/base/xy-dex-base/xy-dex-base-provider.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/base/xy-dex-base/xy-dex-base-provider.ts
@@ -2,5 +2,5 @@ import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 import { XyDexAbstractProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/common/xy-dex-abstract/xy-dex-abstract-provider';
 
 export class XyDexBaseProvider extends XyDexAbstractProvider {
-    public readonly blockchain = BLOCKCHAIN_NAME.ETHEREUM;
+    public readonly blockchain = BLOCKCHAIN_NAME.BASE;
 }
