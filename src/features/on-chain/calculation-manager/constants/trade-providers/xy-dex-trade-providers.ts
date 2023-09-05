7830e39feffd75118e451a06f9961524a1ce0283
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-09-05 17:08:28 +0300

    remove xyDex for astar, cronos, klaytn

--- src/features/on-chain/calculation-manager/constants/trade-providers/xy-dex-trade-providers.ts
+++ src/features/on-chain/calculation-manager/constants/trade-providers/xy-dex-trade-providers.ts
@@ -1,12 +1,9 @@
 import { XyDexArbitrumProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/arbitrum/xy-dex-arbitrum/xy-dex-arbitrum-provider';
-import { XyDexAstarProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/astar-evm/xy-dex-astar/xy-dex-astar-provider';
 import { XyDexAvalancheProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/avalanche/xy-dex-avalanche/xy-dex-avalanche-provider';
 import { XyDexBaseProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/base/xy-dex-base/xy-dex-base-provider';
 import { XyDexBscProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/bsc/xy-dex-bsc/xy-dex-bsc-provider';
-import { XyDexCronosProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/cronos/xy-dex-cronos/xy-dex-cronos-provider';
 import { XyDexEthereumProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/ethereum/xy-dex-ethereum/xy-dex-ethereum-provider';
 import { XyDexFantomProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/fantom/xy-dex-fantom/xy-dex-fantom-provider';
-import { XyDexKlaytnProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/klaytn/xy-dex-klaytn/xy-dex-klaytn-provider';
 import { XyDexLineaProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/linea/xy-dex-linea/xy-dex-linea-provider';
 import { XyDexMoonriverProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/moonriver/xy-dex-moonriver/xy-dex-moonriver-provider';
 import { XyDexOptimismProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/optimism/xy-dex-optimism/xy-dex-optimism-provider';
@@ -19,13 +16,10 @@ export const xyDexTradeProviders = [
     XyDexEthereumProvider,
     XyDexPolygonProvider,
     XyDexFantomProvider,
-    XyDexCronosProvider,
     XyDexAvalancheProvider,
     XyDexArbitrumProvider,
     XyDexOptimismProvider,
-    XyDexAstarProvider,
     XyDexMoonriverProvider,
-    XyDexKlaytnProvider,
     XyDexZksyncProvider,
     XyDexZkevmProvider,
     XyDexLineaProvider,
