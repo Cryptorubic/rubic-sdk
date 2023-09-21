6a53c786b4c60cabd2f3e2a966ff44a1da0faf29
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-09-21 14:38:02 +0300

    add symbiosis api

---
+++ src/features/cross-chain/calculation-manager/providers/symbiosis-provider/models/symbiosis-error.ts
@@ -0,0 +1,14 @@
+export const errorCode = {
+    DEFAULT: 'DEFAULT',
+    NO_ROUTE: 'NO_ROUTE',
+    AMOUNT_TOO_LOW: 'AMOUNT_TOO_LOW',
+    AMOUNT_TOO_HIGH: 'AMOUNT_TOO_HIGH',
+    AMOUNT_LESS_THAN_FEE: 'AMOUNT_LESS_THAN_FEE',
+    NO_TRANSIT_TOKEN: 'NO_TRANSIT_TOKEN',
+    NO_TRANSIT_POO: 'NO_TRANSIT_POO'
+} as const;
+
+export interface SymbiosisError {
+    code: keyof typeof errorCode;
+    message?: string;
+}
