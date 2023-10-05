0c3e0d5c399e3c0b4263614e3ab77599178fc6f9
Demidov Kirill <kirill.demidov.1995@mail.ru>
2023-10-05 17:10:17 +0300

    add new rpc error

--- src/core/blockchain/web3-public-service/web3-public-service.ts
+++ src/core/blockchain/web3-public-service/web3-public-service.ts
@@ -148,7 +148,8 @@ export class Web3PublicService {
                                 e?.message
                                     ?.toLowerCase()
                                     ?.includes('daily request count exceeded') ||
-                                e?.message?.toLowerCase()?.includes('invalid json rpc response')
+                                e?.message?.toLowerCase()?.includes('invalid json rpc response') ||
+                                e?.message?.toLowerCase()?.includes('account suspended')
                             ) {
                                 if (curRpc === rpcProvider.rpcList[0]) {
                                     rpcProvider.rpcList.shift();
