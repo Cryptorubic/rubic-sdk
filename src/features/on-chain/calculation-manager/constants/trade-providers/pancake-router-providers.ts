889b16c2be4c9b1ea535facfdd475111bb1fe5d6
Kirill Demidov <kirill.demidov.1995@mail.ru>
2023-11-10 14:13:23 +0300

    disabled pancake-swap

--- src/features/on-chain/calculation-manager/constants/trade-providers/pancake-router-providers.ts
+++ src/features/on-chain/calculation-manager/constants/trade-providers/pancake-router-providers.ts
@@ -1,9 +1,8 @@
 import { PancakeRouterBscProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/bsc/pancake-router-bsc/pancake-router-bsc-provider';
 import { PancakeRouterEthereumProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/ethereum/pancake-router-ethereum/pancake-router-ethereum-provider';
-import { PancakeRouterPolygonZkEvmProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/polygon-zkevm/pancake-router-polygon-zkevm/pancake-router-polygon-zkevm-provider';
 
 export const pancakeRouterProviders = [
     PancakeRouterBscProvider,
-    PancakeRouterEthereumProvider,
-    PancakeRouterPolygonZkEvmProvider
+    PancakeRouterEthereumProvider
+    // PancakeRouterPolygonZkEvmProvider
 ];
