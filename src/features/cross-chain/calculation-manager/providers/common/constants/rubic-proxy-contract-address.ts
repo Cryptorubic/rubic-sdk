89e4098dca2a14159856edff655c1219a28f97b1
axtezy <axtezy@gmail.com>
2023-11-13 15:43:21 +0100

    Fix stargate

--- src/features/cross-chain/calculation-manager/providers/common/constants/rubic-proxy-contract-address.ts
+++ src/features/cross-chain/calculation-manager/providers/common/constants/rubic-proxy-contract-address.ts
@@ -10,6 +10,7 @@ export const rubicProxyContractAddress: Record<
     (acc, blockchain) => {
         let gateway = '0x3335733c454805df6a77f825f266e136FB4a3333';
         let router = '0x6AA981bFF95eDfea36Bdae98C26B274FfcafE8d3';
+
         if (blockchain === BLOCKCHAIN_NAME.TELOS) {
             router = '0xA2d8CF32C16f070702c45a5686Fdb0a1d7171AAD';
         }
