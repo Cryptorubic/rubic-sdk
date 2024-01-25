0e5349cb431e9f1811a29520e59dd92c14785a5a
pseudoelement <bubamainer@mail.ru>
2024-01-25 13:35:25 +0300

    2175, switch on symbiosis on-chain

--- src/features/on-chain/calculation-manager/models/on-chain-manager-aggregators-types.ts
+++ src/features/on-chain/calculation-manager/models/on-chain-manager-aggregators-types.ts
@@ -1,10 +1,11 @@
 import { LifiProvider } from '../providers/aggregators/lifi/lifi-provider';
 import { OpenOceanProvider } from '../providers/aggregators/open-ocean/open-ocean-provider';
 import { RangoOnChainProvider } from '../providers/aggregators/rango/rango-on-chain-provider';
+import { SymbiosisOnChainProvider } from '../providers/aggregators/symbiosis/symbiosis-on-chain-provider';
 
 export const AGGREGATORS_ON_CHAIN = {
     LIFI: LifiProvider,
     OPEN_OCEAN: OpenOceanProvider,
-    RANGO: RangoOnChainProvider
-    // SYMBIOSIS: SymbiosisOnChainProvider
+    RANGO: RangoOnChainProvider,
+    SYMBIOSIS: SymbiosisOnChainProvider
 } as const;
