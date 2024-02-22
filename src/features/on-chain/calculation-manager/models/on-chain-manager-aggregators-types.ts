e066e71ca24ceef003fcba1e61d53e3f9e0bb97e
pseudoelement <bubamainer@mail.ru>
2024-02-22 18:26:27 +0300

    1966, 5.3.1-alpha.symba.2

--- src/features/on-chain/calculation-manager/models/on-chain-manager-aggregators-types.ts
+++ src/features/on-chain/calculation-manager/models/on-chain-manager-aggregators-types.ts
@@ -4,12 +4,13 @@ import { LifiProvider } from '../providers/aggregators/lifi/lifi-provider';
 import { OdosOnChainProvider } from '../providers/aggregators/odos/odos-on-chain-provider';
 import { OpenOceanProvider } from '../providers/aggregators/open-ocean/open-ocean-provider';
 import { RangoOnChainProvider } from '../providers/aggregators/rango/rango-on-chain-provider';
+import { SymbiosisOnChainProvider } from '../providers/aggregators/symbiosis/symbiosis-on-chain-provider';
 
 export const AGGREGATORS_ON_CHAIN = {
     LIFI: LifiProvider,
     OPEN_OCEAN: OpenOceanProvider,
     RANGO: RangoOnChainProvider,
     ODOS: OdosOnChainProvider,
-    DLN: DlnOnChainProvider
-    // SYMBIOSIS: SymbiosisOnChainProvider
+    DLN: DlnOnChainProvider,
+    SYMBIOSIS: SymbiosisOnChainProvider
 } as const;
