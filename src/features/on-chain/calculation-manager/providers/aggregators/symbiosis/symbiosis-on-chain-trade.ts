fb65366965e883baf745551e0fb92ffaabe59553
PseudoElement <bubamainer@mail.ru>
2024-02-28 12:15:06 +0300

    1966, override getSwapData on symbiosis

--- src/features/on-chain/calculation-manager/providers/aggregators/symbiosis/symbiosis-on-chain-trade.ts
+++ src/features/on-chain/calculation-manager/providers/aggregators/symbiosis/symbiosis-on-chain-trade.ts
@@ -1,6 +1,7 @@
 import BigNumber from 'bignumber.js';
 import {
     LowSlippageDeflationaryTokenError,
+    NotWhitelistedProviderError,
     RubicSdkError,
     SwapRequestError
 } from 'src/common/errors';
@@ -17,6 +18,8 @@ import { ON_CHAIN_TRADE_TYPE, OnChainTradeType } from '../../common/models/on-ch
 import { AggregatorEvmOnChainTrade } from '../../common/on-chain-aggregator/aggregator-evm-on-chain-trade-abstract';
 import { GetToAmountAndTxDataResponse } from '../../common/on-chain-aggregator/models/aggregator-on-chain-types';
 import { SymbiosisTradeStruct } from './models/symbiosis-on-chain-trade-types';
+import { GetContractParamsOptions } from 'src/features/cross-chain/calculation-manager/providers/common/models/get-contract-params-options';
+import { ProxyCrossChainEvmTrade } from 'src/features/cross-chain/calculation-manager/providers/common/proxy-cross-chain-evm-facade/proxy-cross-chain-evm-trade';
 
 export class SymbiosisOnChainTrade extends AggregatorEvmOnChainTrade {
     /* @internal */
@@ -136,5 +139,43 @@ export class SymbiosisOnChainTrade extends AggregatorEvmOnChainTrade {
             tx,
             toAmount: tokenAmountOut.amount
         };
+    }
+
+    protected override async getSwapData(options: GetContractParamsOptions): Promise<unknown[]> {
+        const directTransactionConfig = await this.encodeDirect({
+            ...options,
+            fromAddress: rubicProxyContractAddress[this.from.blockchain].router,
+            supportFee: false,
+            receiverAddress: rubicProxyContractAddress[this.from.blockchain].router
+        });
+        const availableDexs = (
+            await ProxyCrossChainEvmTrade.getWhitelistedDexes(this.from.blockchain)
+        ).map(address => address.toLowerCase());
+
+        const routerAddress = directTransactionConfig.to;
+        const approveAddress = this.providerGateway;
+        const method = directTransactionConfig.data.slice(0, 10);
+
+        if (!availableDexs.includes(routerAddress.toLowerCase())) {
+            throw new NotWhitelistedProviderError(routerAddress, undefined, 'dex');
+        }
+        await ProxyCrossChainEvmTrade.checkDexWhiteList(
+            this.from.blockchain,
+            routerAddress,
+            method
+        );
+
+        return [
+            [
+                routerAddress,
+                approveAddress,
+                this.from.address,
+                this.to.address,
+                this.from.stringWeiAmount,
+                options.extraNativeFee,
+                directTransactionConfig.data,
+                true
+            ]
+        ];
     }
 }
