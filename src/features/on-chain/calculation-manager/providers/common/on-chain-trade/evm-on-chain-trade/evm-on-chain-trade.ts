44122502d9d0dcb9c2855a68e2fe5c3300958ad6
PseudoElement <bubamainer@mail.ru>
2024-03-28 19:41:37 +0300

    2236, try fix useless approve

--- src/features/on-chain/calculation-manager/providers/common/on-chain-trade/evm-on-chain-trade/evm-on-chain-trade.ts
+++ src/features/on-chain/calculation-manager/providers/common/on-chain-trade/evm-on-chain-trade/evm-on-chain-trade.ts
@@ -200,7 +200,7 @@ export abstract class EvmOnChainTrade extends OnChainTrade {
             gasPriceOptions: options?.gasPriceOptions || undefined
         };
 
-        await this.approve(approveOptions, false, this.from.weiAmount);
+        await this.approve(approveOptions, true, this.from.weiAmount);
     }
 
     /**
