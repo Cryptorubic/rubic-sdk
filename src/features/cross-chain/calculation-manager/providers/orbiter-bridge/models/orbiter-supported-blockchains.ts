73d01305c6e14bb8b40680a8e67374a6a1f7a4bd
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-04-03 14:54:38 +0300

    added orbiter support

--- src/features/cross-chain/calculation-manager/providers/orbiter-bridge/models/orbiter-supported-blockchains.ts
+++ src/features/cross-chain/calculation-manager/providers/orbiter-bridge/models/orbiter-supported-blockchains.ts
@@ -16,7 +16,8 @@ export const orbiterSupportedBlockchains = [
     BLOCKCHAIN_NAME.ZETACHAIN,
     BLOCKCHAIN_NAME.BLAST,
     BLOCKCHAIN_NAME.KROMA,
-    BLOCKCHAIN_NAME.STARKNET
+    BLOCKCHAIN_NAME.STARKNET,
+    BLOCKCHAIN_NAME.ZK_FAIR
 ] as const;
 
 export type OrbiterSupportedBlockchain = (typeof orbiterSupportedBlockchains)[number];
