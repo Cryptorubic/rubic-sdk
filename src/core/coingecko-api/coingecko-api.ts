7def35222b967a38da8ec6d58fee5a4d1a57170f
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-04-05 15:49:12 +0300

    upd coingecko-api service

--- src/core/coingecko-api/coingecko-api.ts
+++ src/core/coingecko-api/coingecko-api.ts
@@ -1,7 +1,6 @@
 import BigNumber from 'bignumber.js';
 import { RubicSdkError, TimeoutError } from 'src/common/errors';
 import { Cache } from 'src/common/utils/decorators';
-import pTimeout from 'src/common/utils/p-timeout';
 import { BLOCKCHAIN_NAME, BlockchainName } from 'src/core/blockchain/models/blockchain-name';
 import { BlockchainsInfo } from 'src/core/blockchain/utils/blockchains-info/blockchains-info';
 import { Web3Pure } from 'src/core/blockchain/web3-pure/web3-pure';
@@ -58,7 +57,11 @@ const supportedBlockchains = [
 
 type SupportedBlockchain = (typeof supportedBlockchains)[number];
 
-const API_BASE_URL = 'https://api.coingecko.com/api/v3/';
+interface TokenPriceFromBackend {
+    network: string;
+    address: string;
+    usd_price: number;
+}
 
 /**
  * Works with coingecko api to get tokens prices in usd.
@@ -70,151 +73,54 @@ export class CoingeckoApi {
         return supportedBlockchains.some(supportedBlockchain => supportedBlockchain === blockchain);
     }
 
-    private readonly nativeCoinsCoingeckoIds: Record<SupportedBlockchain, string>;
+    constructor(private readonly httpClient: HttpClient) {}
 
-    private readonly tokenBlockchainId: Record<SupportedBlockchain, string>;
+    private async getTokenPriceFromBackend(
+        blockchain: BlockchainName,
+        tokenAddress: string
+    ): Promise<TokenPriceFromBackend> {
+        return await this.httpClient.get<TokenPriceFromBackend>(
+            `https://dev-tokens.rubic.exchange/api/v1/tokens/price/${blockchain}/${tokenAddress}`
+        );
+    }
 
-    constructor(private readonly httpClient: HttpClient) {
-        this.nativeCoinsCoingeckoIds = {
-            [BLOCKCHAIN_NAME.ETHEREUM]: 'ethereum',
-            [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: 'binancecoin',
-            [BLOCKCHAIN_NAME.POLYGON]: 'matic-network',
-            [BLOCKCHAIN_NAME.AVALANCHE]: 'avalanche-2',
-            [BLOCKCHAIN_NAME.MOONRIVER]: 'moonriver',
-            [BLOCKCHAIN_NAME.FANTOM]: 'fantom',
-            [BLOCKCHAIN_NAME.HARMONY]: 'harmony',
-            [BLOCKCHAIN_NAME.ARBITRUM]: 'ethereum',
-            [BLOCKCHAIN_NAME.AURORA]: 'ethereum',
-            [BLOCKCHAIN_NAME.TELOS]: 'tlos',
-            [BLOCKCHAIN_NAME.BOBA]: 'ethereum',
-            [BLOCKCHAIN_NAME.BITCOIN]: 'bitcoin',
-            [BLOCKCHAIN_NAME.ETHEREUM_POW]: 'ethereum-pow-iou',
-            [BLOCKCHAIN_NAME.KAVA]: 'kava',
-            [BLOCKCHAIN_NAME.OASIS]: 'rose',
-            [BLOCKCHAIN_NAME.METIS]: 'metis-token',
-            [BLOCKCHAIN_NAME.DFK]: 'defi-kingdoms',
-            [BLOCKCHAIN_NAME.KLAYTN]: 'klaytn',
-            [BLOCKCHAIN_NAME.VELAS]: 'velas',
-            [BLOCKCHAIN_NAME.SYSCOIN]: 'syscoin',
-            [BLOCKCHAIN_NAME.ICP]: 'internet-computer',
-            [BLOCKCHAIN_NAME.OPTIMISM]: 'ethereum',
-            [BLOCKCHAIN_NAME.CRONOS]: 'cronos',
-            [BLOCKCHAIN_NAME.OKE_X_CHAIN]: 'okex-chain',
-            [BLOCKCHAIN_NAME.GNOSIS]: 'xdai',
-            [BLOCKCHAIN_NAME.FUSE]: 'fuse',
-            [BLOCKCHAIN_NAME.MOONBEAM]: 'moonbeam',
-            [BLOCKCHAIN_NAME.CELO]: 'celo',
-            [BLOCKCHAIN_NAME.BOBA_BSC]: 'boba',
-            [BLOCKCHAIN_NAME.BITGERT]: 'bitgert',
-            [BLOCKCHAIN_NAME.ETHEREUM_CLASSIC]: 'ethereum-classic',
-            [BLOCKCHAIN_NAME.EOS]: 'eos',
-            [BLOCKCHAIN_NAME.FLARE]: 'flare-network',
-            [BLOCKCHAIN_NAME.IOTEX]: 'iotex',
-            [BLOCKCHAIN_NAME.ONTOLOGY]: 'ontology',
-            [BLOCKCHAIN_NAME.THETA]: 'theta',
-            [BLOCKCHAIN_NAME.XDC]: 'xdc-network',
-            [BLOCKCHAIN_NAME.BITCOIN_CASH]: 'bitcoin-cash',
-            [BLOCKCHAIN_NAME.ZK_SYNC]: 'ethereum',
-            [BLOCKCHAIN_NAME.PULSECHAIN]: 'pulsechain',
-            [BLOCKCHAIN_NAME.LINEA]: 'ethereum',
-            [BLOCKCHAIN_NAME.MANTLE]: 'mantle',
-            [BLOCKCHAIN_NAME.BASE]: 'ethereum',
-            [BLOCKCHAIN_NAME.BLAST]: 'blast',
-            [BLOCKCHAIN_NAME.KROMA]: 'kroma',
-            [BLOCKCHAIN_NAME.ROOTSTOCK]: 'rootstock'
-        };
-
-        this.tokenBlockchainId = {
-            [BLOCKCHAIN_NAME.ETHEREUM]: 'ethereum',
-            [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: 'binance-smart-chain',
-            [BLOCKCHAIN_NAME.POLYGON]: 'polygon-pos',
-            [BLOCKCHAIN_NAME.AVALANCHE]: 'avalanche',
-            [BLOCKCHAIN_NAME.MOONRIVER]: 'moonriver',
-            [BLOCKCHAIN_NAME.FANTOM]: 'fantom',
-            [BLOCKCHAIN_NAME.HARMONY]: 'harmony-shard-0',
-            [BLOCKCHAIN_NAME.ARBITRUM]: 'arbitrum-one',
-            [BLOCKCHAIN_NAME.AURORA]: 'aurora',
-            [BLOCKCHAIN_NAME.TELOS]: 'telos',
-            [BLOCKCHAIN_NAME.BOBA]: 'boba-network',
-            [BLOCKCHAIN_NAME.BITCOIN]: 'bitcoin',
-            [BLOCKCHAIN_NAME.ETHEREUM_POW]: 'ethereum-pow-iou',
-            [BLOCKCHAIN_NAME.KAVA]: 'kava',
-            [BLOCKCHAIN_NAME.OASIS]: 'oasis',
-            [BLOCKCHAIN_NAME.METIS]: 'metis-andromeda',
-            [BLOCKCHAIN_NAME.DFK]: 'defi-kingdoms',
-            [BLOCKCHAIN_NAME.KLAYTN]: 'klaytn',
-            [BLOCKCHAIN_NAME.VELAS]: 'velas',
-            [BLOCKCHAIN_NAME.SYSCOIN]: 'syscoin',
-            [BLOCKCHAIN_NAME.ICP]: 'internet-computer',
-            [BLOCKCHAIN_NAME.OPTIMISM]: 'optimism',
-            [BLOCKCHAIN_NAME.CRONOS]: 'cronos',
-            [BLOCKCHAIN_NAME.OKE_X_CHAIN]: 'okex-chain',
-            [BLOCKCHAIN_NAME.GNOSIS]: 'xdai',
-            [BLOCKCHAIN_NAME.FUSE]: 'fuse',
-            [BLOCKCHAIN_NAME.MOONBEAM]: 'moonbeam',
-            [BLOCKCHAIN_NAME.CELO]: 'celo',
-            [BLOCKCHAIN_NAME.BOBA_BSC]: 'boba',
-            [BLOCKCHAIN_NAME.BITGERT]: 'bitgert',
-            [BLOCKCHAIN_NAME.ETHEREUM_CLASSIC]: 'ethereum-classic',
-            [BLOCKCHAIN_NAME.EOS]: 'eos',
-            [BLOCKCHAIN_NAME.FLARE]: 'flare-network',
-            [BLOCKCHAIN_NAME.IOTEX]: 'iotex',
-            [BLOCKCHAIN_NAME.ONTOLOGY]: 'ontology',
-            [BLOCKCHAIN_NAME.THETA]: 'theta',
-            [BLOCKCHAIN_NAME.XDC]: 'xdc-network',
-            [BLOCKCHAIN_NAME.BITCOIN_CASH]: 'bitcoin-cash',
-            [BLOCKCHAIN_NAME.ZK_SYNC]: 'zksync',
-            [BLOCKCHAIN_NAME.PULSECHAIN]: 'pulsechain',
-            [BLOCKCHAIN_NAME.LINEA]: 'linea',
-            [BLOCKCHAIN_NAME.MANTLE]: 'mantle',
-            [BLOCKCHAIN_NAME.BASE]: 'base',
-            [BLOCKCHAIN_NAME.BLAST]: 'blast-2',
-            [BLOCKCHAIN_NAME.KROMA]: 'kroma',
-            [BLOCKCHAIN_NAME.ROOTSTOCK]: 'rootstock'
-        };
+    private errorHandling(err: unknown): BigNumber {
+        if (err instanceof TimeoutError) {
+            console.debug('[RUBIC SDK]: Timeout Error. Coingecko cannot retrieve token price');
+        } else if ((err as Error)?.message?.includes('Request failed with status code 429')) {
+            console.debug('[RUBIC SDK]: Too many requests. Coingecko cannot retrieve token price');
+        } else {
+            console.debug(err);
+        }
+        return new BigNumber(NaN);
     }
 
     /**
      * Gets price of native coin in usd from coingecko.
      * @param blockchain Supported by {@link supportedBlockchains} blockchain.
+     * @param tokenAddress The address of the requested token.
      */
     @Cache({
         maxAge: 15_000
     })
-    public async getNativeCoinPrice(blockchain: BlockchainName): Promise<BigNumber> {
+    public async getNativeCoinPrice(
+        blockchain: BlockchainName,
+        tokenAddress: string
+    ): Promise<BigNumber> {
         if (!CoingeckoApi.isSupportedBlockchain(blockchain)) {
             throw new RubicSdkError(`Blockchain ${blockchain} is not supported by coingecko-api`);
         }
 
-        const coingeckoId = this.nativeCoinsCoingeckoIds[blockchain];
-
         try {
-            const response = await pTimeout(
-                this.httpClient.get<{ [key: typeof coingeckoId]: { usd: string } }>(
-                    `${API_BASE_URL}simple/price`,
-                    {
-                        params: { ids: coingeckoId, vs_currencies: 'usd' }
-                    }
-                ),
-                3_000
-            );
-            const price = response?.[coingeckoId]?.usd;
-            if (!price) {
+            const response = await this.getTokenPriceFromBackend(blockchain, tokenAddress);
+
+            if (!response.usd_price) {
                 throw new RubicSdkError('Coingecko price is not defined');
             }
 
-            return new BigNumber(price);
+            return new BigNumber(response.usd_price);
         } catch (err: unknown) {
-            if (err instanceof TimeoutError) {
-                console.debug('[RUBIC SDK]: Timeout Error. Coingecko cannot retrieve token price');
-            } else if ((err as Error)?.message?.includes('Request failed with status code 429')) {
-                console.debug(
-                    '[RUBIC SDK]: Too many requests. Coingecko cannot retrieve token price'
-                );
-            } else {
-                console.debug(err);
-            }
-            return new BigNumber(NaN);
+            return this.errorHandling(err);
         }
     }
 
@@ -229,33 +135,18 @@ export class CoingeckoApi {
         address: string;
         blockchain: BlockchainName;
     }): Promise<BigNumber> {
-        const { blockchain } = token;
-        if (!CoingeckoApi.isSupportedBlockchain(blockchain)) {
-            throw new RubicSdkError(`Blockchain ${blockchain} is not supported by coingecko-api`);
+        if (!CoingeckoApi.isSupportedBlockchain(token.blockchain)) {
+            throw new RubicSdkError(
+                `Blockchain ${token.blockchain} is not supported by coingecko-api`
+            );
         }
 
-        const blockchainId = this.tokenBlockchainId[blockchain];
-
         try {
-            const response = await pTimeout(
-                this.httpClient.get<{ market_data: { current_price: { usd: number } } }>(
-                    `${API_BASE_URL}coins/${blockchainId}/contract/${token.address.toLowerCase()}`
-                ),
-                3_000
-            );
+            const response = await this.getTokenPriceFromBackend(token.blockchain, token.address);
 
-            return new BigNumber(response?.market_data?.current_price?.usd || NaN);
+            return new BigNumber(response?.usd_price || NaN);
         } catch (err: unknown) {
-            if (err instanceof TimeoutError) {
-                console.debug('[RUBIC SDK]: Timeout Error. Coingecko cannot retrieve token price');
-            } else if ((err as Error)?.message?.includes('Request failed with status code 429')) {
-                console.debug(
-                    '[RUBIC SDK]: Too many requests. Coingecko cannot retrieve token price'
-                );
-            } else {
-                console.debug(err);
-            }
-            return new BigNumber(NaN);
+            return this.errorHandling(err);
         }
     }
 
@@ -275,7 +166,7 @@ export class CoingeckoApi {
 
         const chainType = BlockchainsInfo.getChainType(token.blockchain);
         if (Web3Pure[chainType].isNativeAddress(token.address)) {
-            return this.getNativeCoinPrice(token.blockchain);
+            return this.getNativeCoinPrice(token.blockchain, token.address);
         }
         return this.getErc20TokenPrice(token);
     }
