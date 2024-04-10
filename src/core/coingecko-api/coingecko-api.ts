6e2550cfa359e5b2f779cd788e2957d0f77504fc
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-04-10 12:21:26 +0300

    fix cg-service api response

--- src/core/coingecko-api/coingecko-api.ts
+++ src/core/coingecko-api/coingecko-api.ts
@@ -1,59 +1,7 @@
 import BigNumber from 'bignumber.js';
-import { RubicSdkError } from 'src/common/errors';
 import { Cache } from 'src/common/utils/decorators';
 import { BLOCKCHAIN_NAME, BlockchainName } from 'src/core/blockchain/models/blockchain-name';
 import { HttpClient } from 'src/core/http-client/models/http-client';
-
-const supportedBlockchains = [
-    BLOCKCHAIN_NAME.ETHEREUM,
-    BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN,
-    BLOCKCHAIN_NAME.POLYGON,
-    BLOCKCHAIN_NAME.AVALANCHE,
-    BLOCKCHAIN_NAME.MOONRIVER,
-    BLOCKCHAIN_NAME.FANTOM,
-    BLOCKCHAIN_NAME.HARMONY,
-    BLOCKCHAIN_NAME.ARBITRUM,
-    BLOCKCHAIN_NAME.AURORA,
-    BLOCKCHAIN_NAME.TELOS,
-    BLOCKCHAIN_NAME.BOBA,
-    BLOCKCHAIN_NAME.BITCOIN,
-    BLOCKCHAIN_NAME.ETHEREUM_POW,
-    BLOCKCHAIN_NAME.KAVA,
-    BLOCKCHAIN_NAME.OASIS,
-    BLOCKCHAIN_NAME.METIS,
-    BLOCKCHAIN_NAME.DFK,
-    BLOCKCHAIN_NAME.KLAYTN,
-    BLOCKCHAIN_NAME.VELAS,
-    BLOCKCHAIN_NAME.SYSCOIN,
-    BLOCKCHAIN_NAME.ICP,
-    BLOCKCHAIN_NAME.OPTIMISM,
-    BLOCKCHAIN_NAME.CRONOS,
-    BLOCKCHAIN_NAME.OKE_X_CHAIN,
-    BLOCKCHAIN_NAME.GNOSIS,
-    BLOCKCHAIN_NAME.FUSE,
-    BLOCKCHAIN_NAME.MOONBEAM,
-    BLOCKCHAIN_NAME.CELO,
-    BLOCKCHAIN_NAME.BOBA_BSC,
-    BLOCKCHAIN_NAME.BITGERT,
-    BLOCKCHAIN_NAME.ETHEREUM_CLASSIC,
-    BLOCKCHAIN_NAME.EOS,
-    BLOCKCHAIN_NAME.FLARE,
-    BLOCKCHAIN_NAME.IOTEX,
-    BLOCKCHAIN_NAME.ONTOLOGY,
-    BLOCKCHAIN_NAME.THETA,
-    BLOCKCHAIN_NAME.XDC,
-    BLOCKCHAIN_NAME.BITCOIN_CASH,
-    BLOCKCHAIN_NAME.ZK_SYNC,
-    BLOCKCHAIN_NAME.PULSECHAIN,
-    BLOCKCHAIN_NAME.LINEA,
-    BLOCKCHAIN_NAME.MANTLE,
-    BLOCKCHAIN_NAME.BASE,
-    BLOCKCHAIN_NAME.BLAST,
-    BLOCKCHAIN_NAME.KROMA,
-    BLOCKCHAIN_NAME.ROOTSTOCK
-] as const;
-
-type SupportedBlockchain = (typeof supportedBlockchains)[number];
 
 interface TokenPriceFromBackend {
     network: string;
@@ -65,12 +13,6 @@ interface TokenPriceFromBackend {
  * Works with coingecko api to get tokens prices in usd.
  */
 export class CoingeckoApi {
-    private static isSupportedBlockchain(
-        blockchain: BlockchainName
-    ): blockchain is SupportedBlockchain {
-        return supportedBlockchains.some(supportedBlockchain => supportedBlockchain === blockchain);
-    }
-
     constructor(private readonly httpClient: HttpClient) {}
 
     @Cache({
@@ -94,12 +36,20 @@ export class CoingeckoApi {
             return result;
         } catch (error) {
             console.debug(error);
-
-            return {
+            const nonPriceToken = {
                 network: blockchain,
                 address: tokenAddress,
                 usd_price: null
             };
+
+            if (
+                error.message.includes('Network not found.') ||
+                error.message.includes('Token not found.')
+            ) {
+                return nonPriceToken;
+            }
+
+            return nonPriceToken;
         }
     }
 
@@ -111,12 +61,6 @@ export class CoingeckoApi {
         address: string;
         blockchain: BlockchainName;
     }): Promise<BigNumber> {
-        if (!CoingeckoApi.isSupportedBlockchain(token.blockchain)) {
-            throw new RubicSdkError(
-                `Blockchain ${token.blockchain} is not supported by coingecko-api`
-            );
-        }
-
         const response = await this.getTokenPriceFromBackend(token.blockchain, token.address);
 
         return new BigNumber(response?.usd_price || NaN);
