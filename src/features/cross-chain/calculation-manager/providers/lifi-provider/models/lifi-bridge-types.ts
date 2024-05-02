db609611c9e809d97c30ea2d7fd39ed39599ae1a
PseudoElement <bubamainer@mail.ru>
2024-05-02 19:17:17 +0300

    2416/2417/2418, fix LIFI_API_CROSS_CHAIN_PROVIDERS

--- src/features/cross-chain/calculation-manager/providers/lifi-provider/models/lifi-bridge-types.ts
+++ src/features/cross-chain/calculation-manager/providers/lifi-provider/models/lifi-bridge-types.ts
@@ -1,26 +1,18 @@
 export const LIFI_API_CROSS_CHAIN_PROVIDERS = {
     ACROSS: 'across',
-    AMAROK: 'connext',
     ARBITRUM: 'arbitrum',
-    AVALANCHE: 'avalanche',
     CBRIDGE: 'cbridge',
-    CONNEXT: 'connext',
     HOP: 'hop',
     HYPHEN: 'hyphen',
-    MULTICHAIN: 'multichain',
     STARGATE: 'stargate',
-    // New
     ALLBRIDGE: 'allbridge',
-    POLYGON_BRIDGE: 'polygon',
     OMNI_BRIDGE: 'omni',
     GNOSIS_BRIDGE: 'gnosis',
     CONNEXT_AMAROK: 'amarok',
     CIRCLE_CELER_BRIDGE: 'celercircle',
-    LI_FUEL: 'lifuel',
-    WORMHOLE: 'portal',
     CELERIM: 'celerim',
-    THORSWAP: 'thorswap',
-    OPTIMISM: 'optimism'
+    OPTIMISM: 'optimism',
+    SYMBIOSIS: 'symbiosis'
 } as const;
 
 export type LifiSubProvider =
