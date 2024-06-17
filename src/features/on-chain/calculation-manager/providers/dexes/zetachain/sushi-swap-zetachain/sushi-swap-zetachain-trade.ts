19513ee4fd994781d146a4f11e5c7bec55572ff0
Andrew <hibebol45@gmail.com>
2024-06-17 11:45:46 +0300

    add UnsupportedReceiverAddress check

--- src/features/on-chain/calculation-manager/providers/dexes/zetachain/sushi-swap-zetachain/sushi-swap-zetachain-trade.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/zetachain/sushi-swap-zetachain/sushi-swap-zetachain-trade.ts
@@ -1,4 +1,7 @@
+import { EncodeTransactionOptions } from 'src/features/common/models/encode-transaction-options';
+import { checkUnsupportedReceiverAddress } from 'src/features/common/utils/check-unsupported-receiver-address';
 import { ON_CHAIN_TRADE_TYPE, OnChainTradeType } from '../../../common/models/on-chain-trade-type';
+import { GetToAmountAndTxDataResponse } from '../../../common/on-chain-aggregator/models/aggregator-on-chain-types';
 import { UniswapV2AbstractTrade } from '../../common/uniswap-v2-abstract/uniswap-v2-abstract-trade';
 import { SUSHI_SWAP_ZETACHAIN_CONTRACT_ADDRESS } from './constants';
 
@@ -8,4 +11,14 @@ export class SushiSwapZetachainTrade extends UniswapV2AbstractTrade {
     }
 
     public readonly dexContractAddress = SUSHI_SWAP_ZETACHAIN_CONTRACT_ADDRESS;
+    protected async getTransactionConfigAndAmount(
+        options: EncodeTransactionOptions
+    ): Promise<GetToAmountAndTxDataResponse> {
+      await checkUnsupportedReceiverAddress(
+        options?.receiverAddress,
+        options?.fromAddress || this.walletAddress
+    )
+    return await super.getTransactionConfigAndAmount(options)
+    }
+
 }
