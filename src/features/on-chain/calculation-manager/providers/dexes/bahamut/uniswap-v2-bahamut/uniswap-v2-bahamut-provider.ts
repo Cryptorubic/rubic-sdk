a98523d6ad7bf695f21e5f7e7bd6f62e98c72e19
Andrew <hibebol45@gmail.com>
2024-07-04 17:35:55 +0300

    update transaction time

--- src/features/on-chain/calculation-manager/providers/dexes/bahamut/uniswap-v2-bahamut/uniswap-v2-bahamut-provider.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/bahamut/uniswap-v2-bahamut/uniswap-v2-bahamut-provider.ts
@@ -1,4 +1,6 @@
 import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
+import { evmProviderDefaultOptions } from '../../common/on-chain-provider/evm-on-chain-provider/constants/evm-provider-default-options';
+import { UniswapV2CalculationOptions } from '../../common/uniswap-v2-abstract/models/uniswap-v2-calculation-options';
 
 import { UniswapV2AbstractProvider } from '../../common/uniswap-v2-abstract/uniswap-v2-abstract-provider';
 import { UNISWAP_V2_BAHAMUT_PROVIDER_CONFIGURATION } from './constants';
@@ -6,6 +8,12 @@ import { UniswapV2BahamutTrade } from './uniswap-v2-bahamut-trade';
 
 export class UniswapV2BahamutProvider extends UniswapV2AbstractProvider<UniswapV2BahamutTrade> {
     public readonly blockchain = BLOCKCHAIN_NAME.BAHAMUT;
+
+    protected readonly defaultOptions: UniswapV2CalculationOptions = {
+        ...evmProviderDefaultOptions,
+        deadlineMinutes: 60,
+        disableMultihops: false
+    };
 
     public readonly UniswapV2TradeClass = UniswapV2BahamutTrade;
 
