fb157307c1c9646d0f7d02e78468a9f5b91c6bee
pseudoelement <bubamainer@mail.ru>
2024-07-08 12:14:24 +0300

    2456, increase min limits for eddy_bridge

--- src/features/cross-chain/calculation-manager/providers/eddy-bridge/constants/swap-limits.ts
+++ src/features/cross-chain/calculation-manager/providers/eddy-bridge/constants/swap-limits.ts
@@ -9,15 +9,15 @@ interface EddySupportedChainLimit {
 
 export const EDDY_BRIDGE_LIMITS: Record<EddyBridgeSupportedTokens, EddySupportedChainLimit> = {
     BNB: {
-        min: new BigNumber(0.002),
+        min: new BigNumber(0.01),
         max: new BigNumber(15)
     },
     ETH: {
-        min: new BigNumber(0.0005),
+        min: new BigNumber(0.0015),
         max: new BigNumber(5)
     },
     ZETA: {
-        min: new BigNumber(1),
+        min: new BigNumber(7),
         max: new BigNumber(15_000)
     }
 };
