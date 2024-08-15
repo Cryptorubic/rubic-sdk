9324ef19d7ea105ef2959da0e0b160e9600dc512
Andrew <hibebol45@gmail.com>
2024-08-15 15:24:48 +0300

    update contract addresses

--- src/features/on-chain/calculation-manager/providers/dexes/bahamut/kujata-bahamut/constants.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/bahamut/kujata-bahamut/constants.ts
@@ -4,13 +4,13 @@ import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 import { UniswapV2ProviderConfiguration } from '../../common/uniswap-v2-abstract/models/uniswap-v2-provider-configuration';
 import { defaultBahamutProviderConfiguration } from '../default-constants';
 
-export const KUJATA_BAHAMUT_CONTRACT_ADDRESS = '0x643b0bBdeD375aC581bE6Ff408F423c13402fC6D';
+export const KUJATA_BAHAMUT_CONTRACT_ADDRESS = '0x90B74764FFfcA7aD47594A14c540794027beB50e';
 
 export const kujataBahamutRoutingProvidersAddresses = [
     wrappedNativeTokensList[BLOCKCHAIN_NAME.BAHAMUT]!.address, //WFTN
-    '0xf0E753D9309BE96c2beEF7Cf524c99799b4FA6fc', //USDC.USDT
+    '0x2A3766bE51f95bece0853d88Ca82D5A26Fd803d5', //USDC.USDT
     '0xd0C1aE39D40DB875679a3329b2d5F71D5929c43f', //USDC.WFTN
-    '0x3f6a26966F2Beb20117cC2370f75d3c736b5ce9e' //WFTN.USDT
+    '0x1F267d46c146039026cD33F6429032B774a07173' //WFTN.USDT
 ];
 
 export const KUJATA_BAHAMUT_PROVIDER_CONFIGURATION: UniswapV2ProviderConfiguration = {
