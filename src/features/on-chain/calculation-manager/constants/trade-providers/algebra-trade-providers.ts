bc762dd9dfb65d8fa231ff828b1c6505e9289113
Andrew <hibebol45@gmail.com>
2024-08-29 13:58:14 +0300

    add camelot dex for gravity

--- src/features/on-chain/calculation-manager/constants/trade-providers/algebra-trade-providers.ts
+++ src/features/on-chain/calculation-manager/constants/trade-providers/algebra-trade-providers.ts
@@ -7,6 +7,7 @@ import { QuickSwapV3Provider } from 'src/features/on-chain/calculation-manager/p
 import { QuickSwapV3PolygonZKEVMProvider } from 'src/features/on-chain/calculation-manager/providers/dexes/polygon-zkevm/quick-swap-v3/quick-swap-v3-provider';
 
 import { AlgebraIntegralProvider } from '../../providers/dexes/arthera-testnet/algebra-integral/algebra-integral-provider';
+import { CamelotGravityProvider } from '../../providers/dexes/gravity/camelot-gravity/camelot-gravity-provider';
 
 export const AlgebraTradeProviders = [
     AlgebraProvider,
@@ -16,6 +17,6 @@ export const AlgebraTradeProviders = [
     CamelotArbitrumProvider,
     BerachainTestnetAlgebraProvider,
     ModeAlgebraProvider,
-    BlastFenixProvider
-    //CamelotGravityProvider
+    BlastFenixProvider,
+    CamelotGravityProvider
 ] as const;
