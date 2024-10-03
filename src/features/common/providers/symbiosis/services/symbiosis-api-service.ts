69289491526576396f478594088c8de402eadcc9
axtezy <axtezy@gmail.com>
2024-10-03 14:50:22 +0200

    Add btc to symbiosis

--- src/features/common/providers/symbiosis/services/symbiosis-api-service.ts
+++ src/features/common/providers/symbiosis/services/symbiosis-api-service.ts
@@ -1,7 +1,5 @@
 import { RubicSdkError } from 'src/common/errors';
 import { Cache } from 'src/common/utils/decorators';
-import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
-import { blockchainId } from 'src/core/blockchain/utils/blockchains-info/constants/blockchain-id';
 import { Injector } from 'src/core/injector/injector';
 import { SymbiosisSwappingParams } from 'src/features/cross-chain/calculation-manager/providers/symbiosis-provider/models/symbiosis-swapping-params';
 import { SymbiosisTradeData } from 'src/features/cross-chain/calculation-manager/providers/symbiosis-provider/models/symbiosis-trade-data';
@@ -45,10 +43,7 @@ export class SymbiosisApiService {
     public static async getCrossChainSwapTx(
         params: SymbiosisSwappingParams
     ): Promise<SymbiosisTradeData> {
-        const url =
-            params.tokenOut.chainId === blockchainId[BLOCKCHAIN_NAME.BITCOIN]
-                ? `${SYMBIOSIS_API_ENDPOINT}/v1/swap`
-                : `${SYMBIOSIS_API_ENDPOINT}/v1/swapping/exact_in?partnerId=rubic`;
+        const url = `${SYMBIOSIS_API_ENDPOINT}/v1/swap`;
 
         const res = await Injector.httpClient.post<SymbiosisTradeData | SymbiosisErrorResponse>(
             url,
