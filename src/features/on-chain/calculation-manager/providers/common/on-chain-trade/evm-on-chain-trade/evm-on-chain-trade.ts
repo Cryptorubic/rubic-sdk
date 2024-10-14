f82ec87bfe487238d6b03d66a47cbd4ef47a6556
Andrew <hibebol45@gmail.com>
2024-10-14 10:16:03 +0300

    fix xy dex swap for base

--- src/features/on-chain/calculation-manager/providers/common/on-chain-trade/evm-on-chain-trade/evm-on-chain-trade.ts
+++ src/features/on-chain/calculation-manager/providers/common/on-chain-trade/evm-on-chain-trade/evm-on-chain-trade.ts
@@ -337,7 +337,7 @@ export abstract class EvmOnChainTrade extends OnChainTrade {
         );
     }
 
-    private async getProxyContractParams(
+    protected async getProxyContractParams(
         options: EncodeTransactionOptions
     ): Promise<ContractParams> {
         const swapData = await this.getSwapData(options);
@@ -379,7 +379,7 @@ export abstract class EvmOnChainTrade extends OnChainTrade {
         };
     }
 
-    private static getReferrerAddress(referrer: string | undefined): string {
+    protected static getReferrerAddress(referrer: string | undefined): string {
         if (referrer) {
             return '0x' + utf8ToHex(referrer).slice(2, 42).padStart(40, '0');
         }
