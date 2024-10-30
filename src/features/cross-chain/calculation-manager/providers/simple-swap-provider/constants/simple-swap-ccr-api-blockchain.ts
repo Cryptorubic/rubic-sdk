6a8357dd41d1575244314f7acf5d3d062a82b6ed
Andrew <hibebol45@gmail.com>
2024-10-30 10:57:50 +0300

    fix comments

--- src/features/cross-chain/calculation-manager/providers/simple-swap-provider/constants/simple-swap-ccr-api-blockchain.ts
+++ src/features/cross-chain/calculation-manager/providers/simple-swap-provider/constants/simple-swap-ccr-api-blockchain.ts
@@ -2,23 +2,24 @@ import { BLOCKCHAIN_NAME } from 'src/core/blockchain/models/blockchain-name';
 
 export const simpleSwapEvmSupportedChains = {
     [BLOCKCHAIN_NAME.ETHEREUM]: 'eth',
+    [BLOCKCHAIN_NAME.ETHEREUM_POW]: 'ethw',
     [BLOCKCHAIN_NAME.BINANCE_SMART_CHAIN]: 'bsc',
     [BLOCKCHAIN_NAME.POLYGON]: 'matic',
-    [BLOCKCHAIN_NAME.AVALANCHE]: 'avaxc',
+    [BLOCKCHAIN_NAME.AVALANCHE]: 'avaxc', // avax
     [BLOCKCHAIN_NAME.MOONRIVER]: 'movr',
-    [BLOCKCHAIN_NAME.FANTOM]: 'ftm',
+    [BLOCKCHAIN_NAME.FANTOM]: 'fantom',
     [BLOCKCHAIN_NAME.HARMONY]: 'one',
     [BLOCKCHAIN_NAME.ARBITRUM]: 'arbitrum',
     [BLOCKCHAIN_NAME.OPTIMISM]: 'optimism',
-    [BLOCKCHAIN_NAME.TELOS]: 'tlos', // tlosevm ?
-    [BLOCKCHAIN_NAME.CRONOS]: 'croevm', // cronos ?
+    [BLOCKCHAIN_NAME.TELOS]: 'tlosevm',
+    [BLOCKCHAIN_NAME.CRONOS]: 'cro', // cronos ?
     [BLOCKCHAIN_NAME.MOONBEAM]: 'glmr',
     [BLOCKCHAIN_NAME.KAVA]: 'kavaevm',
-    [BLOCKCHAIN_NAME.KLAYTN]: 'klaytn', // klay
+    [BLOCKCHAIN_NAME.KLAYTN]: 'klay',
     [BLOCKCHAIN_NAME.VELAS]: 'vlx',
-    [BLOCKCHAIN_NAME.SYSCOIN]: 'sysnevm',
+    [BLOCKCHAIN_NAME.SYSCOIN]: 'sysevm',
     [BLOCKCHAIN_NAME.IOTEX]: 'iotx',
-    [BLOCKCHAIN_NAME.LINEA]: 'linea',
+    [BLOCKCHAIN_NAME.LINEA]: 'lineaeth', // linea
     [BLOCKCHAIN_NAME.BASE]: 'base',
     [BLOCKCHAIN_NAME.MANTLE]: 'mnt',
     [BLOCKCHAIN_NAME.SCROLL]: 'scroll',
@@ -30,9 +31,13 @@ export const simpleSwapEvmSupportedChains = {
     [BLOCKCHAIN_NAME.CORE]: 'core',
     [BLOCKCHAIN_NAME.MANTA_PACIFIC]: 'manta',
     [BLOCKCHAIN_NAME.CELO]: 'celo',
-    [BLOCKCHAIN_NAME.ZK_LINK]: 'zklnova',
-    [BLOCKCHAIN_NAME.ZK_SYNC]: 'zkSync',
-    [BLOCKCHAIN_NAME.OASIS]: 'rose'
+    [BLOCKCHAIN_NAME.ZK_LINK]: 'zkl',
+    [BLOCKCHAIN_NAME.ZK_SYNC]: 'zksera',
+    [BLOCKCHAIN_NAME.OASIS]: 'rose',
+    [BLOCKCHAIN_NAME.METIS]: 'metis',
+    [BLOCKCHAIN_NAME.STARKNET]: 'starknet',
+    [BLOCKCHAIN_NAME.PULSECHAIN]: 'pulse',
+    [BLOCKCHAIN_NAME.MERLIN]: 'merlbtc'
 };
 
 export const simpleSwapApiChain = {
@@ -41,22 +46,18 @@ export const simpleSwapApiChain = {
     // NON-EVM
     [BLOCKCHAIN_NAME.FLARE]: 'flr',
     [BLOCKCHAIN_NAME.THETA]: 'theta',
-    [BLOCKCHAIN_NAME.SOLANA]: 'sol', // solana
+    [BLOCKCHAIN_NAME.SOLANA]: 'sol',
     [BLOCKCHAIN_NAME.NEAR]: 'near',
     [BLOCKCHAIN_NAME.BITCOIN]: 'btc',
     [BLOCKCHAIN_NAME.TRON]: 'trx',
     [BLOCKCHAIN_NAME.ICP]: 'icp',
     [BLOCKCHAIN_NAME.CARDANO]: 'ada',
-    [BLOCKCHAIN_NAME.ALGORAND]: 'algo',
+    [BLOCKCHAIN_NAME.ALGORAND]: 'algo', // algousdt
     [BLOCKCHAIN_NAME.APTOS]: 'apt', // aptos
-    [BLOCKCHAIN_NAME.ARDOR]: 'ardr',
-    [BLOCKCHAIN_NAME.ARK]: 'ark',
     [BLOCKCHAIN_NAME.ASTAR]: 'astr',
     [BLOCKCHAIN_NAME.COSMOS]: 'atom',
-    [BLOCKCHAIN_NAME.BSV]: 'bsv',
-    [BLOCKCHAIN_NAME.CASPER]: 'cspr',
     [BLOCKCHAIN_NAME.DASH]: 'dash',
-    [BLOCKCHAIN_NAME.DOGECOIN]: 'dogechain',
+    [BLOCKCHAIN_NAME.DOGECOIN]: 'doge',
     [BLOCKCHAIN_NAME.POLKADOT]: 'dot',
     [BLOCKCHAIN_NAME.FLOW]: 'flow',
     [BLOCKCHAIN_NAME.IOTA]: 'iota',
@@ -71,13 +72,14 @@ export const simpleSwapApiChain = {
     [BLOCKCHAIN_NAME.STELLAR]: 'xlm',
     [BLOCKCHAIN_NAME.MONERO]: 'xmr',
     [BLOCKCHAIN_NAME.TEZOS]: 'xtz',
-    [BLOCKCHAIN_NAME.HORIZEN]: 'zen', // horizen eon ?
     [BLOCKCHAIN_NAME.FILECOIN]: 'fil',
     [BLOCKCHAIN_NAME.EOS]: 'eos',
     [BLOCKCHAIN_NAME.ONTOLOGY]: 'ont',
     [BLOCKCHAIN_NAME.XDC]: 'xdc',
     [BLOCKCHAIN_NAME.ZILLIQA]: 'zil',
-    [BLOCKCHAIN_NAME.KAVA_COSMOS]: 'kava'
+    [BLOCKCHAIN_NAME.KAVA_COSMOS]: 'kava',
+    [BLOCKCHAIN_NAME.MINA_PROTOCOL]: 'mina',
+    [BLOCKCHAIN_NAME.SIA]: 'sc'
 };
 
 export type SimpleSwapCcrSupportedChain = keyof typeof simpleSwapApiChain;
