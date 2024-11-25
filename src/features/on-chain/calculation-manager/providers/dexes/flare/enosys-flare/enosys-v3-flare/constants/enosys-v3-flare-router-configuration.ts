9465dca42730a69081193d0b637fd119d8f26430
Andrew <hibebol45@gmail.com>
2024-11-25 09:17:05 +0300

    add new liquidity poolfor enosys v3

--- src/features/on-chain/calculation-manager/providers/dexes/flare/enosys-flare/enosys-v3-flare/constants/enosys-v3-flare-router-configuration.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/flare/enosys-flare/enosys-v3-flare/constants/enosys-v3-flare-router-configuration.ts
@@ -9,7 +9,19 @@ import {
 /**
  * Most popular tokens in uni v3 to use in a route.
  */
-const tokensSymbols = ['eUSDT', 'WFLR', 'sFLR', 'eETH', 'eQNT', 'HLN', 'APS', 'BNZ'] as const;
+const tokensSymbols = [
+    'eUSDT',
+    'WFLR',
+    'sFLR',
+    'eETH',
+    'eQNT',
+    'HLN',
+    'APS',
+    'BNZ',
+    'USDC.e',
+    'USDT',
+    'cUSDX'
+] as const;
 
 type TokenSymbol = (typeof tokensSymbols)[number];
 
@@ -21,7 +33,10 @@ const routerTokens: Record<TokenSymbol, string> = {
     eQNT: '0x60fDC7B744E886e96Aa0DEf5f69eE440dB9d8c77',
     HLN: '0x140D8d3649Ec605CF69018C627fB44cCC76eC89f',
     APS: '0xfF56Eb5b1a7FAa972291117E5E9565dA29bc808d',
-    BNZ: '0xfD3449E8Ee31117a848D41Ee20F497a9bCb53164'
+    BNZ: '0xfD3449E8Ee31117a848D41Ee20F497a9bCb53164',
+    'USDC.e': '0xfbda5f676cb37624f28265a144a48b0d6e87d3b6',
+    USDT: '0x0b38e83b86d491735feaa0a791f65c2b99535396',
+    cUSDX: '0xfe2907dfa8db6e320cdbf45f0aa888f6135ec4f8'
 };
 
 const routerLiquidityPools: UniswapV3RouterLiquidityPool<TokenSymbol>[] = [
@@ -54,6 +69,12 @@ const routerLiquidityPools: UniswapV3RouterLiquidityPool<TokenSymbol>[] = [
         tokenSymbolA: 'sFLR',
         tokenSymbolB: 'HLN',
         fee: 3000
+    },
+    {
+        poolAddress: '0x25B4f3930934F0A3CbB885C624EcEe75a2917144',
+        tokenSymbolA: 'sFLR',
+        tokenSymbolB: 'WFLR',
+        fee: 500
     },
     {
         poolAddress: '0xf06eeBF7A66C80760Bd8343A6BCe84c9D61879ee',
@@ -104,10 +125,10 @@ const routerLiquidityPools: UniswapV3RouterLiquidityPool<TokenSymbol>[] = [
         fee: 3000
     },
     {
-        poolAddress: '0x02C6b5B1fbE01Da872E21f9Dab1B980933B0EF27',
+        poolAddress: '0x3De8C22790549F323313d513Df2A1cb30b28B9A4',
         tokenSymbolA: 'HLN',
         tokenSymbolB: 'WFLR',
-        fee: 3000
+        fee: 100
     },
     {
         poolAddress: '0x87E0E33558c8e8EAE3c1E9EB276e05574190b48a',
@@ -138,6 +159,54 @@ const routerLiquidityPools: UniswapV3RouterLiquidityPool<TokenSymbol>[] = [
         tokenSymbolA: 'WFLR',
         tokenSymbolB: 'BNZ',
         fee: 3000
+    },
+    {
+        poolAddress: '0x164857e59CdE0848910Dac791650Da52db736dc2',
+        tokenSymbolA: 'WFLR',
+        tokenSymbolB: 'USDC.e',
+        fee: 3000
+    },
+    {
+        poolAddress: '0x4a885Ed3EC3F3E657440422e93C15A73EdF6A909',
+        tokenSymbolA: 'USDT',
+        tokenSymbolB: 'WFLR',
+        fee: 3000
+    },
+    {
+        poolAddress: '0x9ba3E13E581de917A6E5E0a70037628679d0dA3C',
+        tokenSymbolA: 'USDT',
+        tokenSymbolB: 'USDC.e',
+        fee: 500
+    },
+    {
+        poolAddress: '0x130Bc3C6Ab530A56B7B7dc926A8E33C1E0eCA472',
+        tokenSymbolA: 'cUSDX',
+        tokenSymbolB: 'USDC.e',
+        fee: 3000
+    },
+    {
+        poolAddress: '0x04Af25eF7AEf113a5Da24124f69deDb8a88e8656',
+        tokenSymbolA: 'WFLR',
+        tokenSymbolB: 'APS',
+        fee: 10000
+    },
+    {
+        poolAddress: '0x507Ba799d81c8e7848FA4e0c966bF96a7e28B5CD',
+        tokenSymbolA: 'WFLR',
+        tokenSymbolB: 'eETH',
+        fee: 3000
+    },
+    {
+        poolAddress: '0xA6e32137CaCB5EFf9846B9af8e435abea6a15CC0',
+        tokenSymbolA: 'HLN',
+        tokenSymbolB: 'APS',
+        fee: 10000
+    },
+    {
+        poolAddress: '0x7Bd497D2af21e25F28F57bdd1Caf2d453B5a7272',
+        tokenSymbolA: 'HLN',
+        tokenSymbolB: 'eQNT',
+        fee: 10000
     }
 ];
 
