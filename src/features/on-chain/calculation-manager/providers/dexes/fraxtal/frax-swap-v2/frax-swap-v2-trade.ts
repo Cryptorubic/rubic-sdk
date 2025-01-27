6f718e63b6484f7cce285ec7b17b30adcbf7c0b0
Andrew <hibebol45@gmail.com>
2025-01-27 12:27:44 +0300

    enable proxy for fraxtal, fix fraxSwap V2 calculation

--- src/features/on-chain/calculation-manager/providers/dexes/fraxtal/frax-swap-v2/frax-swap-v2-trade.ts
+++ src/features/on-chain/calculation-manager/providers/dexes/fraxtal/frax-swap-v2/frax-swap-v2-trade.ts
@@ -1,6 +1,13 @@
+import { EvmBlockchainName } from 'src/core/blockchain/models/blockchain-name';
 import { ON_CHAIN_TRADE_TYPE, OnChainTradeType } from '../../../common/models/on-chain-trade-type';
 import { UniswapV2AbstractTrade } from '../../common/uniswap-v2-abstract/uniswap-v2-abstract-trade';
 import { FRAXSWAP_V2_CONTRACT_ADDRESS } from './constants';
+import { ExtendedRoutesMethodArguments } from '../../common/uniswap-v2-abstract/models/route-method-arguments';
+import { ContractMulticallResponse } from 'src/core/blockchain/web3-public-service/web3-public/models/contract-multicall-response';
+import { Injector } from 'src/core/injector/injector';
+import { Exact } from '../../../common/on-chain-trade/evm-on-chain-trade/models/exact';
+import { AbiItem } from 'web3-utils';
+import { FRAXSWAP_V2_ABI } from './frax-swap-v2-abi';
 
 export class FraxSwapV2Trade extends UniswapV2AbstractTrade {
     public static get type(): OnChainTradeType {
@@ -8,4 +15,22 @@ export class FraxSwapV2Trade extends UniswapV2AbstractTrade {
     }
 
     public readonly dexContractAddress = FRAXSWAP_V2_CONTRACT_ADDRESS;
+
+    /** @internal */
+    public static readonly contractAbi: AbiItem[] = FRAXSWAP_V2_ABI;
+
+    public static override callForRoutes(
+        blockchain: EvmBlockchainName,
+        exact: Exact,
+        routesMethodArguments: ExtendedRoutesMethodArguments
+    ): Promise<ContractMulticallResponse<string[]>[]> {
+        const web3Public = Injector.web3PublicService.getWeb3Public(blockchain);
+        const methodName = exact === 'input' ? 'getAmountsOutWithTwamm' : 'getAmountsInWithTwamm';
+        return web3Public.multicallContractMethod<string[]>(
+            this.getDexContractAddress(blockchain),
+            this.contractAbi,
+            methodName,
+            routesMethodArguments
+        );
+    }
 }
