d4854ff63ae6734aecb1f334ca765a943839a619
Andrew <hibebol45@gmail.com>
2025-03-14 17:33:51 +0300

    update customDeflationTokenList

--- src/features/deflation-token-manager/models/custom-deflation-token-list.ts
+++ src/features/deflation-token-manager/models/custom-deflation-token-list.ts
@@ -1,5 +1,6 @@
 import { BLOCKCHAIN_NAME, EvmBlockchainName } from 'src/core/blockchain/models/blockchain-name';
 
 export const customDeflationTokeList: Partial<Record<EvmBlockchainName, string[]>> = {
-    [BLOCKCHAIN_NAME.FLARE]: ['0xc18f99ce6dd6278be2d3f1e738ed11623444ae33']
+    [BLOCKCHAIN_NAME.FLARE]: ['0xc18f99ce6dd6278be2d3f1e738ed11623444ae33'],
+    [BLOCKCHAIN_NAME.POLYGON]: ['0x7b3bd12675c6b9d6993eb81283cb68e6eb9260b5']
 };
