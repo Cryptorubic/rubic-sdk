58629751dd676319be843004ec8d962108bff48e
Andrew <hibebol45@gmail.com>
2025-05-19 12:27:29 +0300

    2007, fix different amount error

--- src/features/ws-api/rubic-api-service.ts
+++ src/features/ws-api/rubic-api-service.ts
@@ -5,7 +5,8 @@ import {
     WsQuoteRequestInterface,
     WsQuoteResponseInterface
 } from '@cryptorubic/core';
-import { catchError, concatMap, from, fromEvent, map, Observable, of } from 'rxjs';
+import BigNumber from 'bignumber.js';
+import { catchError, concatMap, filter, from, fromEvent, map, Observable, of } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
 import {
     InsufficientFundsError,
@@ -149,6 +150,14 @@ export class RubicApiService {
                 type: string;
             }
         >(this.client, 'events').pipe(
+            filter(({ trade }) => {
+                if (!trade) {
+                    return true;
+                }
+                const { from } = trade?.tokens!;
+
+                return new BigNumber(from?.amount!).eq(this.latestQuoteParams?.srcTokenAmount!);
+            }),
             concatMap(wsResponse => {
                 const { trade, total, calculated, data } = wsResponse;
                 let promise: Promise<
