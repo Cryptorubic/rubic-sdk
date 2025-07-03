ee3bcacce839d6477d93885a7e0b4ba501edf7dc
Andrew <hibebol45@gmail.com>
2025-07-03 13:12:36 +0300

    2294, remove kroma chain

--- src/features/cross-chain/calculation-manager/providers/meson-provider/models/meson-provider-types.ts
+++
@@ -1,9 +0,0 @@
-import BigNumber from 'bignumber.js';
-
-export interface FetchedMesonTradeInfo {
-    mesonFee: string;
-    sourceAssetString: string;
-    targetAssetString: string;
-    min: BigNumber;
-    max: BigNumber;
-}
--- src/features/cross-chain/calculation-manager/providers/retro-bridge/services/retro-bridge-api-service.ts
+++ src/features/cross-chain/calculation-manager/providers/retro-bridge/services/retro-bridge-api-service.ts
@@ -2,18 +2,6 @@ import { TX_STATUS } from 'src/core/blockchain/web3-public-service/web3-public/m
 import { Injector } from 'src/core/injector/injector';
 import { TxStatusData } from 'src/features/common/status-manager/models/tx-status-data';
 
-import {
-    RetroBridgeQuoteResponse,
-    RetroBridgeQuoteSendParams,
-    RetroBridgeTxResponse,
-    RetroBridgeTxSendParams
-} from '../models/retro-bridge-quote-send-params';
-import { RetroBridgeToken } from '../models/retro-bridge-token';
-import {
-    RETRO_BRIDGE_TX_STATUS,
-    RetroBridgeStatusResponse
-} from '../models/retro-bridge-tx-status';
-
 interface SignMessage {
     message: {
         value: string;
@@ -23,42 +11,6 @@ export class RetroBridgeApiService {
     private static readonly RETRO_BRIDGE_API_ENDPOINT = 'https://backend.retrobridge.io/api';
 
     private static API_KEY = 'rubic';
-
-    public static async getTokenLimits(
-        fromBlockchain: string,
-        toBlockchain: string,
-        fromToken: string,
-        toToken: string
-    ): Promise<{ min_send: number; max_send: number }> {
-        const { data } = await Injector.httpClient.get<{
-            data: {
-                min_send: number;
-                max_send: number;
-            };
-        }>(`${this.RETRO_BRIDGE_API_ENDPOINT}/bridge/limits`, {
-            params: {
-                source_chain: fromBlockchain,
-                destination_chain: toBlockchain,
-                asset_from: fromToken,
-                asset_to: toToken
-            }
-        });
-        return data;
-    }
-
-    public static async getQuote(
-        params: RetroBridgeQuoteSendParams
-    ): Promise<RetroBridgeQuoteResponse> {
-        const { data } = await Injector.httpClient.get<{ data: RetroBridgeQuoteResponse }>(
-            `${this.RETRO_BRIDGE_API_ENDPOINT}/bridge/quote`,
-            {
-                params: {
-                    ...params
-                }
-            }
-        );
-        return data;
-    }
 
     public static async getMessageToAuthWallet(): Promise<string> {
         const { data } = await Injector.httpClient.get<{ data: SignMessage }>(
@@ -100,64 +52,5 @@ export class RetroBridgeApiService {
             }
         );
         return message;
-    }
-
-    public static async createTransaction(
-        params: RetroBridgeTxSendParams,
-        networkType: string
-    ): Promise<RetroBridgeTxResponse> {
-        const { data } = await Injector.httpClient.post<{ data: RetroBridgeTxResponse }>(
-            `${this.RETRO_BRIDGE_API_ENDPOINT}/bridge/execute`,
-            params,
-            {
-                headers: {
-                    'network-type': networkType,
-                    'api-key': this.API_KEY
-                },
-                withCredentials: true
-            }
-        );
-        return data;
-    }
-
-    public static async getTxStatus(transactionId: string): Promise<TxStatusData> {
-        const { data } = await Injector.httpClient.get<RetroBridgeStatusResponse>(
-            `${this.RETRO_BRIDGE_API_ENDPOINT}/bridge/${transactionId}/info`
-        );
-        const txStatus = data.status.toLowerCase();
-
-        if (txStatus === RETRO_BRIDGE_TX_STATUS.COMPLETED) {
-            return {
-                hash: data.destination_tx_hash,
-                status: TX_STATUS.SUCCESS
-            };
-        }
-        if (
-            txStatus === RETRO_BRIDGE_TX_STATUS.SEND_FAILED ||
-            txStatus === RETRO_BRIDGE_TX_STATUS.REJECTED
-        ) {
-            return {
-                hash: data.source_tx_hash,
-                status: TX_STATUS.FAIL
-            };
-        }
-        return {
-            hash: null,
-            status: TX_STATUS.PENDING
-        };
-    }
-
-    public static getTokenList(
-        srcChain: string,
-        dstChain: string
-    ): Promise<{ data: RetroBridgeToken[] }> {
-        return Injector.httpClient.get(
-            `${this.RETRO_BRIDGE_API_ENDPOINT}/assets/?source_chain=${srcChain}&destination_chain=${dstChain}`,
-            {
-                headers: {
-                    'api-key': this.API_KEY
-                }
-            }
-        );
     }
 }
